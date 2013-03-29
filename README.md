fab279b1ade5dc7beaaaded47b0533b6f6765cc5
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-03-29 15:43:16 +0100

    Merge joelkuiper/addis changes to schema

--- README.md
+++ README.md
@@ -4,5 +4,12 @@ TrialVerse
 Running
 -------
 
-	sudo apt-get install openjdk-7-jdk maven postgresql postgresql-client
+	sudo apt-get install openjdk-7-jdk maven postgresql postgresql-client libsaxonb-java
 	mvn jetty:run
+
+	sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'develop'"
+	sudo -u postgres createuser -S -D -R trialverse
+	sudo -u postgres psql -c "ALTER USER trialverse WITH PASSWORD 'develop'"
+
+	cd dbms
+	./rebuild.sh
--- dbms/rebuild.sh
+++ dbms/rebuild.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-DB_PORT=2345
-SSH_PORT=2222
+PRIV_USER=postgres
+PRIV_PWD=develop
+
+APP_USER=trialverse
+APP_PWD=develop
+
+PORT=5432
 HOST=localhost
+USER=trialverse
+PASSWORD=develop
+DATABASE=trialverse
 
-PGPASSWORD=develop psql -h $HOST -p $DB_PORT -U postgres -c  "SELECT pg_terminate_backend(pg_stat_activity.procpid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'addis'";
-PGPASSWORD=develop psql -h $HOST -p $DB_PORT -U postgres -c "DROP DATABASE IF EXISTS addis"
-psql -h $HOST -p $DB_PORT -U addis -d postgres -c "CREATE DATABASE addis ENCODING 'utf-8' OWNER addis"
-psql -h $HOST -p $DB_PORT -U addis -f structure.sql
+PGPASSWORD=$PRIV_PWD psql -h $HOST -p $PORT -U $PRIV_USER -c  "SELECT pg_terminate_backend(pg_stat_activity.procpid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$DATABASE'";
+PGPASSWORD=$PRIV_PWD psql -h $HOST -p $PORT -U $PRIV_USER -c "DROP DATABASE IF EXISTS $DATABASE"
+PGPASSWORD=$PRIV_PWD psql -h $HOST -p $PORT -U $PRIV_USER -d postgres -c "CREATE DATABASE $DATABASE ENCODING 'utf-8' OWNER $USER"
 
-echo "Updating documentation"
-ssh addis@$HOST -p $SSH_PORT "postgresql_autodoc";
-scp -P $SSH_PORT addis@$HOST:~/addis.\* documentation/;
-dot -Tpdf documentation/addis.dot -o documentation/addis.pdf;
+PGPASSWORD=$APP_PWD psql -h $HOST -p $PORT -U $APP_USER -f structure.sql
+
+#echo "Updating documentation"
+#ssh addis@$HOST -p $SSH_PORT "postgresql_autodoc";
+#scp -P $SSH_PORT addis@$HOST:~/addis.\* documentation/;
+#dot -Tpdf documentation/addis.dot -o documentation/addis.pdf;
 
 echo "Adding example files"
 ./transform.sh "depressionExample" "Hansen 2005" "Depression dataset based on the Hansen et al. (2005) systematic review"
-psql -h $HOST -p $DB_PORT -U addis -f depressionExample.sql
+PGPASSWORD=$APP_PWD psql -h $HOST -p $PORT -U $APP_USER -f depressionExample.sql
 
 ./transform.sh "hypertensionExample" "Edarbi EPAR" "Hypertension dataset based on the Edarbi EPAR"
-psql -h $HOST -p $DB_PORT -U addis -f hypertensionExample.sql
-
+PGPASSWORD=$APP_PWD psql -h $HOST -p $PORT -U $APP_USER -f hypertensionExample.sql
 
