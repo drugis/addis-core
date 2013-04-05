bef1dcdecfaf7dc96a3ec7ae2f65b25a8cfa4fa3
Joël Kuiper <me@joelkuiper.eu>
2013-04-05 13:16:07 +0200

    Add project lombok
    The verbosity, it was driving me /mad/!

--- src/main/java/org/drugis/trialverse/model/Study.java
+++ src/main/java/org/drugis/trialverse/model/Study.java
@@ -6,30 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
-public class Study {
-	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
-	@Column
-	private String name;
-	@Column
-	private String title;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
+public @Data class Study {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
+	@Column private String name;
+	@Column private String title;
 }
