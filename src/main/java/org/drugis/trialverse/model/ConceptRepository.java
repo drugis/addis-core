478c15ebc1feb621b0fc6acf0ea53e5bad8ba1e8
Joël Kuiper <me@joelkuiper.eu>
2013-04-08 17:34:23 +0200

    Rename route for findByType

--- src/main/java/org/drugis/trialverse/model/ConceptRepository.java
+++ src/main/java/org/drugis/trialverse/model/ConceptRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.repository.annotation.RestResource;
 
-@RestResource(path = "concepts", rel = "concepts")
+@RestResource(path = "concepts")
 public interface ConceptRepository extends JpaRepository<Concept, UUID> {
+	
+	@RestResource(path="type", rel="types")
 	public List<Concept> findByType(@Param("type") ConceptType type);
 }
