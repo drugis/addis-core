a27d400548bd2d2ffcfe639634c5a0b7d926d3fc
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-09-13 11:19:18 +0200

    Retrieve instances for defined indication

--- sparql/js/controllers.js
+++ sparql/js/controllers.js
@@ -7,10 +7,19 @@ controller('IndicationController', ['$scope', 'SparqlService', function($scope, 
   $scope.selected = [];
   $scope.searchString = "cardio*";
 
+  var extractValues = function(result) {
+    var bindings = result.data.results.bindings;
+    return _.map(bindings, function(binding) {
+      return _.object(_.map(_.pairs(binding), function(obj) {
+        return [ obj[0], obj[1].value ];
+      }));
+    });
+  };
+
   $scope.query = function() {
     $sparql.query('disease-search', { query: $scope.searchString }).then(
       function(result) {
-        $scope.data = result.data.results.bindings;
+        $scope.data = extractValues(result);
         $scope.terms = $scope.data;
         $scope.narrower = [];
         $scope.broader = [];
@@ -24,13 +33,27 @@ controller('IndicationController', ['$scope', 'SparqlService', function($scope, 
       });
   };
 
+  $scope.count = function() {
+    $sparql.query('instances', { terms : $scope.selected }).then(
+      function(result) {
+        $scope.termCount = extractValues(result)[0].count;
+        $scope.sparql = result.query;
+      },
+      function(error) {
+        console.log(error.cause);
+        if (error.query) {
+          $scope.sparql = error.query;
+        }
+      });
+  };
+
   $scope.getAdjacent = function(term) { 
     $scope.clicked = term;
-    var uri = term.uri.value;
+    var uri = term.uri;
     var resultsFn = function(assignTo) {
       return function(result) {
         $scope.sparql = result.query;
-        $scope.data = result.data.results.bindings;
+        $scope.data = extractValues(result);
         $scope[assignTo] = $scope.data;
       };
     };
@@ -47,8 +70,10 @@ controller('IndicationController', ['$scope', 'SparqlService', function($scope, 
   };
 
   $scope.add = function(term) {
-    if (!_.contains($scope.selected, term)) {
-      $scope.selected.push(term);
+    if (!_.find($scope.selected, function(obj) { return obj.uri === term.uri; })) {
+      var t = _.clone(term);
+      t.transitive = "exact";
+      $scope.selected.push(t);
     }
   };
 
