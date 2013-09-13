d0fea26a30e739abd12e20680c18ea52a0857746
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-09-13 15:03:14 +0200

    Add drugs, adverse events, endpoints

--- sparql/js/app.js
+++ sparql/js/app.js
@@ -5,6 +5,9 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/indication', {templateUrl: 'partials/indication.html', controller: 'IndicationController'});
+    $routeProvider.when('/drug', {templateUrl: 'partials/indication.html', controller: 'DrugController'});
+    $routeProvider.when('/adverseEvent', {templateUrl: 'partials/indication.html', controller: 'AdverseEventController'});
+    $routeProvider.when('/endpoint', {templateUrl: 'partials/indication.html', controller: 'EndpointController'});
     $routeProvider.otherwise({redirectTo: '/indication'});
   }])
   .config(['$httpProvider', function($httpProvider) {
