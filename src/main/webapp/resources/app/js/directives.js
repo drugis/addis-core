eeb5790720676ae807324e7d2f19fde551f9193c
Connor Stroomberg <c.stroomberg@gmail.com>
2014-02-13 12:54:34 +0100

    implement alert

--- src/main/webapp/resources/app/js/directives.js
+++ src/main/webapp/resources/app/js/directives.js
@@ -2,5 +2,7 @@
 define(function (require) {
   var angular = require('angular');
   return angular.module('addis.directives', [])
-    .directive('modal', require('directives/modalDirective'));
+    .directive('modal', require('directives/modalDirective'))
+    .directive('alert', require('directives/alertDirective'))
+    ;
 });
