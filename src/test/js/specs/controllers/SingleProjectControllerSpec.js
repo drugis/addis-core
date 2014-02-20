ad5e15f3ff9aa98f4f915f9c6fb158ae64e2cc03
Daan Reid <osmosisch@gmail.com>
2014-02-20 12:26:35 +0100

    Add breadcrumbs; hide until resource populating them is loaded to prevent layout issues.

--- src/test/js/specs/controllers/SingleProjectControllerSpec.js
+++ src/test/js/specs/controllers/SingleProjectControllerSpec.js
@@ -1,22 +1,31 @@
-define(['angular', 'angular-mocks', 'controllers'], function () {
-  describe("SingleProjectController", function () {
+define(['angular', 'angular-mocks'], function () {
+  describe('SingleProjectController', function () {
     beforeEach(module('addis.controllers'));
-      var scope, projectsService,
-       mockProject = {id: 1, name: 'projectName', description: 'testDescription', namespace: 'testNamespace'};
+    var scope, projectsService, deferred
+      mockProject = {id: 1, name: 'projectName', description: 'testDescription', namespace: 'testNamespace'};
 
-      beforeEach(inject(function ($controller) {
-        var mockStateParams = {id: mockProject.id};
-        projectsService = jasmine.createSpyObj('projectsService', ['get']);
-        projectsService.get.andReturn(mockProject);
-        scope = {};
+    beforeEach(inject(function ($controller, $q, $rootScope) {
+      var mockStateParams = {id: mockProject.id};
+      projectsService = jasmine.createSpyObj('projectsService', ['get']);
+      projectsService.get.andReturn(mockProject);
+      scope = $rootScope;
+      deferred = $q.defer();
+      mockProject.$promise = deferred.promise;
 
-        $controller('SingleProjectController', {$scope: scope, 'ProjectsService': projectsService, $stateParams: mockStateParams});
-      }));
+      $controller('SingleProjectController', {$scope: scope, 'ProjectsService': projectsService, $stateParams: mockStateParams});
+    }));
 
-      it('should place project information on the scope', function() {
-        expect(projectsService.get).toHaveBeenCalledWith({id: mockProject.id});
-        expect(scope.project).toEqual(mockProject);
-      });
+    it('should place project information on the scope', function () {
+      expect(projectsService.get).toHaveBeenCalledWith({id: mockProject.id});
+      expect(scope.project).toEqual(mockProject);
+    });
+
+    it('should tell the scope whether the resource is loaded', function() {
+      expect(scope.loading.loaded).toBeFalsy();
+      deferred.resolve();
+      scope.$apply();
+      expect(scope.loading.loaded).toBeTruthy();
+    });
 
   });
 });
