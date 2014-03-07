5636bf66fab7d2486b1af9abd6ad38f8894d88f3
Connor Stroomberg <c.stroomberg@gmail.com>
2014-03-07 09:16:18 +0100

    bug fix , route should be 'interventions' instead of 'intervention'

--- src/main/webapp/resources/app/js/services/interventionService.js
+++ src/main/webapp/resources/app/js/services/interventionService.js
@@ -2,7 +2,7 @@
 define(['angular', 'angular-resource'], function (angular, angularResource) {
   var dependencies = ['$resource'];
   var InterventionService = function ($resource) {
-    return $resource('/projects/:projectId/intervention/:interventionId', {projectId: '@projectId', interventionId: '@id'});
+    return $resource('/projects/:projectId/interventions/:interventionId', {projectId: '@projectId', interventionId: '@id'});
   };
   return dependencies.concat(InterventionService);
 });
