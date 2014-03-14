8f3852621444e73bbf230d88a146128bb9d69636
Daan Reid <osmosisch@gmail.com>
2014-03-14 17:12:27 +0100

    save selected outcomes ( front-end)

--- src/main/webapp/resources/app/js/controllers/analysisController.js
+++ src/main/webapp/resources/app/js/controllers/analysisController.js
@@ -1,15 +1,35 @@
 'use strict';
-define([], function() {
+define(['underscore'], function() {
   var dependencies = ['$scope', '$stateParams', '$q', 'ProjectsService', 'AnalysisService', 'OutcomeService'];
   var AnalysisController = function($scope, $stateParams, $q, ProjectsService, AnalysisService, OutcomeService) {
     $scope.loading = {loaded : false};
     $scope.project = ProjectsService.get($stateParams);
     $scope.analysis = AnalysisService.get($stateParams);
-     $scope.outcomes = OutcomeService.query($stateParams);
+    $scope.outcomes = OutcomeService.query($stateParams);
+
+    var idsToOutcomes = function(selectedOutcomeIds, outcomes) {
+      var parsed = _.map(selectedOutcomeIds, function(outcomeId) {
+        return _.find(outcomes, function(outcome) {
+          return outcome && outcome.id === parseInt(outcomeId, 10);
+        });
+      });
+    };
+
+    var outcomesToIds = function(outcomes) {
+      _.map(outcomes, function(outcome) {
+        return outcome.id.toString();
+      });
+    };
 
     $q.all([$scope.project.$promise, $scope.analysis.$promise]).then(function () {
       $scope.loading.loaded = true;
+      $scope.selectedOutcomes = outcomesToIds($scope.analysis.selectedOutcomes);
+      $scope.$watchCollection('selectedOutcomeIds', function() {
+        $scope.analysis.selectedOutcomes = idsToOutcomes($scope.selectedOutcomeIds, $scope.outcomes);
+        $scope.analysis.$save();
+      });
     });
+
 
   }
   return dependencies.concat(AnalysisController);
--- src/main/webapp/resources/app/js/services/analysisService.js
+++ src/main/webapp/resources/app/js/services/analysisService.js
@@ -2,7 +2,7 @@
 define(['angular', 'angular-resource'], function (angular, angularResource) {
   var dependencies = ['$resource'];
   var AnalysisService= function ($resource) {
-    return $resource('/projects/:projectId/analyses/:analysisId', {projectId: '@projectId', analysisId: '@analysisId'});
+    return $resource('/projects/:projectId/analyses/:analysisId', {projectId: '@projectId', analysisId: '@id'});
   };
   return dependencies.concat(AnalysisService);
 });
