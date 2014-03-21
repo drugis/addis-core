82300be7406bcf7d0a8bd3b9f74304beff9bfee0
connor.stroomberg <c.stroomberg@gmail.com>
2014-03-21 08:59:27 +0100

    setup for disable editing on select2

--- src/main/webapp/resources/app/js/controllers/analysisController.js
+++ src/main/webapp/resources/app/js/controllers/analysisController.js
@@ -1,38 +1,65 @@
 'use strict';
 define(['underscore'], function () {
   var dependencies = ['$scope', '$stateParams', '$q',
-    'ProjectsService', 'AnalysisService', 'OutcomeService', 'InterventionService', 'Select2UtilService'
+    'ProjectsService', 'AnalysisService', 'OutcomeService', 'InterventionService', 'Select2UtilService', 'TrialverseStudyService'
   ];
-  var AnalysisController = function ($scope, $stateParams, $q,
-    ProjectsService, AnalysisService, OutcomeService, InterventionService, Select2UtilService) {
+  var AnalysisController = function ($scope, $stateParams, $q, $window,
+    ProjectsService, AnalysisService, OutcomeService, InterventionService,
+    Select2UtilService, TrialverseStudyService) {
 
     $scope.loading = {
       loaded: false
     };
+
+    $scope.editMode = {
+      allowEditing: false
+    };
+
     $scope.project = ProjectsService.get($stateParams);
     $scope.analysis = AnalysisService.get($stateParams);
+
     $scope.outcomes = OutcomeService.query($stateParams);
     $scope.interventions = InterventionService.query($stateParams);
+
+
     $scope.selectedOutcomeIds = [];
     $scope.selectedInterventionIds = [];
 
-    $q.all([$scope.project.$promise, $scope.analysis.$promise]).then(function () {
+    $q.all([
+      $scope.project.$promise,
+      $scope.analysis.$promise
+    ]).then(function () {
       $scope.loading.loaded = true;
+      $scope.editMode.allowEditing = $window.config.user.id === $scope.project.owner.id;
+
+      $scope.studies = TrialverseStudyService.query({
+        id: $scope.project.trialverseId
+      });
       $scope.selectedOutcomeIds = Select2UtilService.objectsToIds($scope.analysis.selectedOutcomes);
       $scope.selectedInterventionIds = Select2UtilService.objectsToIds($scope.analysis.selectedInterventions);
 
-      $scope.$watchCollection('selectedOutcomeIds', function () {
-        $scope.analysis.selectedOutcomes = Select2UtilService.idsToObjects($scope.selectedOutcomeIds, $scope.outcomes);
-        $scope.analysis.$save();
+      $scope.$watchCollection('selectedOutcomeIds', function (newValue, oldValue) {
+        if (newValue.length !== $scope.analysis.selectedOutcomes.length) {
+          $scope.analysis.selectedOutcomes = Select2UtilService.idsToObjects($scope.selectedOutcomeIds, $scope.outcomes);
+          $scope.analysis.$save();
+        }
       });
 
-      $scope.$watchCollection('selectedInterventionIds', function () {
-        $scope.analysis.selectedInterventions = Select2UtilService.idsToObjects($scope.selectedInterventionIds, $scope.interventions);
-        $scope.analysis.$save();
+      $scope.$watchCollection('selectedInterventionIds', function (newValue, oldValue) {
+        if (newValue.length !== $scope.analysis.selectedInterventions.length) {
+          $scope.analysis.selectedInterventions = Select2UtilService.idsToObjects($scope.selectedInterventionIds, $scope.interventions);
+          $scope.analysis.$save();
+        }
       });
+
+      $scope.$watch('analysis.studyId', function (newValue, oldValue) {
+        if (oldValue !== newValue) {
+          $scope.analysis.$save();
+        }
+      });
+
     });
 
-
-  }
+  };
   return dependencies.concat(AnalysisController);
 });
