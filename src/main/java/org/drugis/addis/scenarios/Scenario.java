97ebb35c2ef97018b619449ed51c1623160e6cdd
Connor Stroomberg <c.stroomberg@gmail.com>
2014-04-03 16:59:34 +0200

    gettin' scenario with id

---
+++ src/main/java/org/drugis/addis/scenarios/Scenario.java
@@ -0,0 +1,16 @@
+package org.drugis.addis.scenarios;
+
+/**
+ * Created by connor on 3-4-14.
+ */
+public class Scenario {
+  private Integer id;
+
+  public Scenario(Integer id) {
+    this.id = id;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+}
---
+++ src/main/java/org/drugis/addis/scenarios/controller/ScenarioController.java
@@ -0,0 +1,30 @@
+package org.drugis.addis.scenarios.controller;
+
+import org.drugis.addis.base.AbstractAddisCoreController;
+import org.drugis.addis.scenarios.Scenario;
+import org.drugis.addis.scenarios.repository.ScenarioRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+
+/**
+ * Created by connor on 3-4-14.
+ */
+@Controller
+@Transactional("ptmAddisCore")
+public class ScenarioController extends AbstractAddisCoreController {
+
+  @Inject
+  ScenarioRepository scenarioRepository;
+
+  @RequestMapping(value = "/projects/{projectId}/analyses/{analysisId}/scenarios/{scenarioId}", method = RequestMethod.GET)
+  @ResponseBody
+  public Scenario get(@PathVariable Integer scenarioId) {
+    return scenarioRepository.get(scenarioId);
+  }
+}
