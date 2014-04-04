f97498d63f1224d21e50f6c7a6da3c3824fddd63
Daan Reid <osmosisch@gmail.com>
2014-04-04 15:56:59 +0200

    update database schema

--- database.sql
+++ database.sql
@@ -77,3 +77,16 @@ CREATE TABLE Analysis_Interventions (
   FOREIGN KEY(AnalysisId) REFERENCES Analysis(id),
   FOREIGN KEY(InterventionId) REFERENCES Intervention(id)
 );
+
+-- changeset reidd:2
+
+CREATE TABLE Scenario (id SERIAL NOT NULL,
+    						workspace INT NOT NULL,
+    						title VARCHAR NOT NULL,
+    						state VARCHAR NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(workspace) REFERENCES Analysis(id));
+
+-- changeset reidd:3
+
+ALTER TABLE Analysis ADD problem VARCHAR NULL
