63387bd44d78c9cc6bdf6b1b39c4c3f27f3f4a1a
Connor Stroomberg <c.stroomberg@gmail.com>
2014-05-14 14:02:02 +0200

    add new files for: rename analysisService to singleStudyBenefitRiskAnalysisService

---
+++ src/main/webapp/resources/app/js/services/singleStudyBenefitRiskAnalysisService.js
@@ -0,0 +1,65 @@
+'use strict';
+define(['angular'], function() {
+  var dependencies = ['$location', '$stateParams', '$q', 'ProblemResource', 'ScenarioResource'];
+  var SingleStudyBenefitRiskAnalysisService = function($location, $stateParams, $q, ProblemResource, ScenarioResource) {
+
+    var getDefaultScenario = function() {
+      return ScenarioResource
+        .query($stateParams)
+        .$promise
+        .then(function(scenarios) {
+          return scenarios[0];
+        });
+    };
+
+    var validateAnalysis = function(analysis) {
+      return analysis.selectedInterventions.length >= 2 &&
+        analysis.selectedOutcomes.length >= 2;
+    };
+
+    var keyify = function(input) {
+      var output =  input.replace(/[^a-zA-Z0-9 ]/g, '').replace(/ /g, '-').toLowerCase();
+      return output;
+    };
+
+    var verifyCell = function(performanceEntry, outcome, intervention) {
+      var result = performanceEntry.criterion === keyify(outcome.name) && performanceEntry.alternative === keyify(intervention.name);
+      return result;
+    };
+
+    var findPerformanceEntry = function(performanceTable, outcome, intervention) {
+      var result =  _.find(performanceTable, function(performanceEntry) {
+        return verifyCell(performanceEntry, outcome, intervention);
+      });
+      return result;
+    };
+
+    var checkAllInterventionsForOutcome = function(performanceTable, outcome, selectedInterventions) {
+      var result =  _.every(selectedInterventions, function(intervention) {
+        return findPerformanceEntry(performanceTable, outcome, intervention);
+      });
+      return result;
+    };
+
+    var validateProblem = function(analysis, problem) {
+      var selectedOutcomes = analysis.selectedOutcomes;
+      var selectedInterventions = analysis.selectedInterventions;
+      return _.every(selectedOutcomes, function(outcome) {
+        return checkAllInterventionsForOutcome(problem.performanceTable, outcome, selectedInterventions);
+      });
+    };
+
+    var getProblem = function() {
+      return ProblemResource.get($stateParams).$promise;
+    };
+
+    return {
+      getProblem: getProblem,
+      validateProblem: validateProblem,
+      getDefaultScenario: getDefaultScenario,
+      validateAnalysis: validateAnalysis,
+      keyify: keyify
+    };
+  };
+  return dependencies.concat(SingleStudyBenefitRiskAnalysisService);
+});
---
+++ src/test/js/specs/services/singleStudyBenefitRiskAnalysisServiceSpec.js
@@ -0,0 +1,202 @@
+define(['angular', 'angular-mocks', 'services'], function() {
+  describe("The singleStudyBenefitRisk Analysis service", function() {
+
+    var mockProblemResource,
+      mockScenarioResource,
+      location;
+
+    describe('getProblem', function() {
+
+      beforeEach(module('addis.services'));
+      beforeEach(module('addis.resources'));
+
+      beforeEach(function() {
+
+        mockProblemResource = jasmine.createSpyObj('ProblemResource', ['get']);
+        mockScenarioResource = jasmine.createSpyObj('ScenarioResource', ['query']);
+        mockLocation = jasmine.createSpyObj('$location', ['url']);
+
+        module('addis', function($provide) {
+          $provide.value('ProblemResource', mockProblemResource);
+          $provide.value('ScenarioResource', mockScenarioResource);
+          $provide.value('$location', mockLocation);
+        });
+      });
+
+      it('should return a problemResoure',
+        inject(function($rootScope, $q, $location, SingleStudyBenefitRiskAnalysisService) {
+          var mockProblem = {
+            $promise: 'promise'
+          };
+          mockProblemResource.get.and.returnValue(mockProblem);
+          expect(SingleStudyBenefitRiskAnalysisService.getProblem()).toEqual(mockProblem.$promise);
+        }));
+
+    });
+
+    describe('getDefaultScenario', function() {
+
+      beforeEach(module('addis.services'));
+      beforeEach(module('addis.resources'));
+
+      beforeEach(function() {
+
+        mockProblemResource = jasmine.createSpyObj('ProblemResource', ['get']);
+        mockScenarioResource = jasmine.createSpyObj('ScenarioResource', ['query']);
+        mockLocation = jasmine.createSpyObj('$location', ['url']);
+
+        module('addis', function($provide) {
+          $provide.value('ProblemResource', mockProblemResource);
+          $provide.value('ScenarioResource', mockScenarioResource);
+          $provide.value('$location', mockLocation);
+        });
+      });
+
+      it('should return the default scenario when getDefaultScenario is called',
+        inject(function($rootScope, $q, $location, SingleStudyBenefitRiskAnalysisService) {
+          var defaultScenario,
+            scenariosDeferred = $q.defer(),
+            scenarios = [{
+              id: 13
+            }];
+
+          scenarios.$promise = scenariosDeferred.promise;
+          mockScenarioResource.query.and.returnValue(scenarios);
+
+          defaultScenario = SingleStudyBenefitRiskAnalysisService.getDefaultScenario();
+          expect(defaultScenario.then).not.toBeNull();
+          defaultScenario.then(function(result) {
+            defaultScenario = result;
+          });
+          scenariosDeferred.resolve(scenarios);
+          $rootScope.$apply();
+
+          expect(defaultScenario).toEqual(scenarios[0]);
+        }));
+
+
+    });
+
+    describe("validateAnalysis", function() {
+      beforeEach(function() {
+        stateParams = jasmine.createSpyObj('stateParams', ['foo']);
+        module('addis', function($provide) {
+          $provide.value('$stateParams', stateParams);
+        });
+      });
+
+      it("should reject analyses that contain fewer than two selectedInterventions and two selectedOutcomes",
+        inject(function($rootScope, $q, $location, $stateParams, SingleStudyBenefitRiskAnalysisService) {
+          var invalidAnalysis = {
+            selectedInterventions: [],
+            selectedOutcomes: []
+          };
+          expect(SingleStudyBenefitRiskAnalysisService.validateAnalysis(invalidAnalysis)).toBeFalsy();
+
+          invalidAnalysis = {
+            selectedInterventions: [1],
+            selectedOutcomes: []
+          };
+          expect(SingleStudyBenefitRiskAnalysisService.validateAnalysis(invalidAnalysis)).toBeFalsy();
+
+          invalidAnalysis = {
+            selectedInterventions: [],
+            selectedOutcomes: [1]
+          };
+          expect(SingleStudyBenefitRiskAnalysisService.validateAnalysis(invalidAnalysis)).toBeFalsy();
+
+          invalidAnalysis = {
+            selectedInterventions: [1, 2, 3],
+            selectedOutcomes: []
+          };
+          expect(SingleStudyBenefitRiskAnalysisService.validateAnalysis(invalidAnalysis)).toBeFalsy();
+
+          invalidAnalysis = {
+            selectedInterventions: [],
+            selectedOutcomes: [1, 2, 3]
+          };
+          expect(SingleStudyBenefitRiskAnalysisService.validateAnalysis(invalidAnalysis)).toBeFalsy();
+
+          var validAnalysis = {
+            selectedInterventions: [1, 2],
+            selectedOutcomes: [1, 2]
+          };
+          expect(SingleStudyBenefitRiskAnalysisService.validateAnalysis(invalidAnalysis)).toBeFalsy();
+
+          validAnalysis = {
+            selectedInterventions: [1, 2, 3],
+            selectedOutcomes: [1, 2, 3]
+          };
+          expect(SingleStudyBenefitRiskAnalysisService.validateAnalysis(invalidAnalysis)).toBeFalsy();
+        }));
+    });
+
+    describe("validateProblem", function() {
+      beforeEach(function() {
+        stateParams = jasmine.createSpyObj('stateParams', ['foo']);
+        module('addis', function($provide) {
+          $provide.value('$stateParams', stateParams);
+        });
+      });
+
+      it("should reject problems that do not have performance data for each combination of criterion and alternative",
+        inject(function(SingleStudyBenefitRiskAnalysisService) {
+          var analysis = {
+            selectedOutcomes: [{
+              name: 'outcome1'
+            }, {
+              name: 'outcome2'
+            }],
+            selectedInterventions: [{
+              name: 'intervention1'
+            }, {
+              name: 'intervention2'
+            }]
+          };
+          var problem = {
+            performanceTable: [{}]
+          };
+          expect(SingleStudyBenefitRiskAnalysisService.validateProblem(analysis, problem)).toBeFalsy();
+          problem = {
+            performanceTable: [{
+              'alternative': 'intervention1',
+              'criterion': 'outcome1'
+            }, {
+              'alternative': 'intervention1',
+              'criterion': 'outcome2'
+            }, {
+              'alternative': 'intervention2',
+              'criterion': 'outcome1'
+            }]
+          };
+          expect(SingleStudyBenefitRiskAnalysisService.validateProblem(analysis, problem)).toBeFalsy();
+          problem.performanceTable.push({
+            'alternative': 'intervention2',
+            'criterion': 'outcome2'
+          });
+          expect(SingleStudyBenefitRiskAnalysisService.validateProblem(analysis, problem)).toBeTruthy();
+        }));
+    });
+
+    describe('keyify', function() {
+      beforeEach(function() {
+        stateParams = jasmine.createSpyObj('stateParams', ['foo']);
+        module('addis', function($provide) {
+          $provide.value('$stateParams', stateParams);
+        });
+      });
+
+      it('should remove non-alphanumeric characters', inject(function(SingleStudyBenefitRiskAnalysisService) {
+        expect(SingleStudyBenefitRiskAnalysisService.keyify("i'm/not$alpha&numeric")).toBe('imnotalphanumeric');
+      }));
+
+      it('should convert whitespace to dashes', inject(function(SingleStudyBenefitRiskAnalysisService) {
+        expect(SingleStudyBenefitRiskAnalysisService.keyify('a horse a horse my')).toBe('a-horse-a-horse-my');
+      }));
+
+      it('should convert upper case to lowercase', inject(function(SingleStudyBenefitRiskAnalysisService) {
+        expect(SingleStudyBenefitRiskAnalysisService.keyify('IM NOT SHOUTING')).toBe('im-not-shouting');
+      }));
+    });
+  });
+});
