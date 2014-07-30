437b24438e8179c9a484d36eafec257715ff6024
connor.stroomberg <c.stroomberg@gmail.com>
2014-07-30 21:43:34 +0200

    remove empty controller

--- src/main/webapp/resources/app/js/app.js
+++ src/main/webapp/resources/app/js/app.js
@@ -112,11 +112,6 @@ define(
             templateUrl: baseTemplatePath + 'projects.html',
             controller: 'ProjectsController'
           })
-          .state('create-project', {
-            url: 'create-project',
-            templateUrl: baseTemplatePath + 'createProject.html',
-            controller: 'CreateProjectController'
-          })
           .state('project', {
             url: '/projects/:projectId',
             templateUrl: baseTemplatePath + 'project.html',
--- src/main/webapp/resources/app/js/controllers.js
+++ src/main/webapp/resources/app/js/controllers.js
@@ -3,7 +3,6 @@ define(function (require) {
   var angular = require('angular');
   return angular.module('addis.controllers', [])
     .controller('ProjectsController', require('controllers/projectsController'))
-    .controller('CreateProjectController', require('controllers/createProjectController'))
     .controller('SingleProjectController', require('controllers/singleProjectController'))
     .controller('AnalysisController', require('controllers/analysisController'))
     .controller('SingleStudyBenefitRiskAnalysisController', require('controllers/singleStudyBenefitRiskAnalysisController'))
