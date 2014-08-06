e32017ae7d661c946362342d1d6aac1be471e9dc
Connor Stroomberg <c.stroomberg@gmail.com>
2014-08-06 09:52:21 +0200

    tests for create project controller

---
+++ src/test/js/specs/controllers/createProjectControllerSpec.js
@@ -0,0 +1,61 @@
+'use strict';
+define(['angular', 'angular-mocks', 'controllers'],
+  function() {
+    describe("The createProjectController", function() {
+      var scope, ctrl, projectResource, trialverseResource,
+        modalSpy;
+
+      beforeEach(module('addis.controllers'));
+
+      beforeEach(inject(function($controller) {
+        trialverseResource = jasmine.createSpyObj('trialverseResource', ['query']);
+        projectResource = jasmine.createSpyObj('projectResource', ['save']);
+        modalSpy = jasmine.createSpyObj('createProjectModal', ['open']);
+
+        trialverseResource.query.and.returnValue([{
+          key: 'val',
+          uid: 'uid'
+        }]);
+
+        scope = {
+          createProjectModal: modalSpy
+        };
+        ctrl = $controller('CreateProjectController', {
+          $scope: scope,
+          $state: {},
+          'ProjectResource': projectResource,
+          'TrialverseResource': trialverseResource
+        });
+      }));
+
+      it("should make a list of trialverse namespaces available from the trialVerseResource", function() {
+        expect(scope.namespaces.length).toBe(1);
+      });
+
+      it('should place the selected namespace on createProjectModal and  show the modal dialog', function() {
+        var selectedNamespace = {
+          uid: 'uid',
+          name: 'name'
+        };
+        scope.showCreateProjectModal(selectedNamespace);
+        expect(scope.createProjectModal.selectedNamespace).toBe(selectedNamespace);
+        expect(scope.createProjectModal.open).toHaveBeenCalled();
+      });
+
+      it("should make a save call when createProject is called, and clear the scope model so that the form is cleared", function() {
+        var newProject = {
+          name: 'testName',
+          description: 'testDescription'
+        };
+        var selectedNamespace = {
+          uid: 'uid',
+          name: 'name'
+        };
+        scope.showCreateProjectModal(selectedNamespace);
+        scope.model = newProject;
+        scope.createProject(newProject);
+        expect(projectResource.save).toHaveBeenCalled();
+        expect(scope.model).toEqual({});
+      });
+    });
+  });
