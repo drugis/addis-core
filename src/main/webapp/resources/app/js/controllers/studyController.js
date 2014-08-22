71b04788c8262aa495283328e50867434c543366
Connor Stroomberg <c.stroomberg@gmail.com>
2014-08-22 09:49:35 +0200

    fix table rendering to handle flattened outcomes, outcome objects are flattened into rows before rendering

--- src/main/webapp/resources/app/js/controllers/studyController.js
+++ src/main/webapp/resources/app/js/controllers/studyController.js
@@ -15,9 +15,35 @@ define([], function() {
     $scope.studyEpochs = StudyEpochResource.query($stateParams);
     $scope.treatmentActivities = StudyTreatmentActivityResource.query($stateParams);
 
-    $scope.studyPopulationCharacteristics = StudyPopulationCharacteristicsResource.get($stateParams);
-    $scope.studyEndpoints = StudyEndpointsResource.get($stateParams);
-    $scope.studyAdverseEvents = StudyAdverseEventsResource.get($stateParams);
+    $scope.studyPopulationCharacteristics = StudyPopulationCharacteristicsResource
+      .get($stateParams, function(populationCharacteristics){
+        $scope.populationCharacteristicsRows = flattenOutcomesToTableRows(populationCharacteristics);
+      });
+
+    $scope.studyEndpoints = StudyEndpointsResource.get($stateParams, function(endpoints) {
+      $scope.endpointRows = flattenOutcomesToTableRows(endpoints);
+    });
+
+    $scope.studyAdverseEvents = StudyAdverseEventsResource.get($stateParams, function(adverseEvents) {
+      $scope.adverseEventsRows = flattenOutcomesToTableRows(adverseEvents);
+    });
+
+    function flattenOutcomesToTableRows (outcomes) {
+      var rows = [];
+      _.each(outcomes, function(outcome) {
+        _.each(outcome.studyDataMoments, function(moment) {
+          var row = {};
+          row.studyDataTypeLabel = outcome.studyDataTypeLabel;
+          row.relativeToAnchorOntology = moment.relativeToAnchorOntology;
+          row.relativeToEpochLabel = moment.relativeToEpochLabel;
+          row.timeOffsetDuration = moment.timeOffsetDuration;
+          row.studyDataArmValues = moment.studyDataArmValues;
+          rows.push(row);
+        });
+
+      });
+      return rows;
+    }
 
     $scope.cellTreatments = function(epochUid, armUid) {
       return _.filter($scope.treatmentActivities, function(activity) {
@@ -28,19 +54,6 @@ define([], function() {
           return activity;
         }
       });
-    };
-
-    $scope.flattenMoments = function(outcomes) {
-      _.each(outcomes, function(outcome) {
-        outcome.momentAndValues = [];
-        _.each(outcome.studyDataMoments, function(moment) {
-          outcome.relativeToAnchorOntology = moment.relativeToAnchorOntology;
-          outcome.relativeToEpochLabel = moment.relativeToEpochLabel;
-          outcome.timeOffsetDuration = moment.timeOffsetDuration;
-          outcome.momentAndValues = outcome.momentAndValues.concat(moment.studyDataArmValues);
-        });
-      });
-      return outcomes;
     };
 
 
