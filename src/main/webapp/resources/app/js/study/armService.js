6f0eb954aa5cc1bfc04cebf4c05eb25b5bf86073
Connor Stroomberg <c.stroomberg@gmail.com>
2014-12-10 10:02:41 +0100

    move query to external .spaql  file

--- src/main/webapp/resources/app/js/study/armService.js
+++ src/main/webapp/resources/app/js/study/armService.js
@@ -1,28 +1,34 @@
 'use strict';
 define([],
   function() {
-    var dependencies = ['StudyService', '$filter'];
-    var ArmService = function(StudyService, $filter) {
+    var dependencies = ['$resource', 'StudyService'];
+    var ArmService = function($resource, StudyService) {
+
+      var query ;
+      getResource('editArmWithComment.sparql');
+
+
+      function getResource(name) {
+        return $resource('app/js/study/directives/arm/sparql/:name', {name: name}, {
+          'get': {
+            method: 'get',
+            transformResponse: function(data) {
+              return {
+                data: data 
+              };
+            }
+          }
+        }).get(function(result) {
+          query = result;
+        });
+      }
 
       function edit(arm) {
-        var editQuery =
-          ' PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>' +
-          ' PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ' +
+        var editArmQuery = query.data.replace(/\$armURI/g, arm.armURI.value)
+                              .replace('$newArmLabel', arm.label.value)
+                              .replace('$newArmComment', arm.comment.value);
 
-          ' DELETE {  ' +
-          ' <' + arm.armURI.value + '> rdfs:label ?label .' +
-          ' <' + arm.armURI.value + '> rdfs:comment ?comment .' +
-          ' }' +
-          ' INSERT { ' +
-          ' <' + arm.armURI.value + '> rdfs:label "' + arm.label.value + '" .' +
-          ' <' + arm.armURI.value + '> rdfs:comment "' + arm.comment.value + '" .' +
-          ' }' +
-          ' WHERE { ' +
-          ' <' + arm.armURI.value + '> rdfs:label ?label .' +
-          ' <' + arm.armURI.value + '> rdfs:comment ?comment . ' +
-          ' }';
-
-        return StudyService.doQuery(editQuery);
+        return StudyService.doQuery(editArmQuery);
       }
 
 
