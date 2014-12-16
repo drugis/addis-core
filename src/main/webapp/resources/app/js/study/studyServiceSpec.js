eea8e4d13a335c85421ecc8566194b43840c266b
connor.stroomberg <c.stroomberg@gmail.com>
2014-12-16 10:40:19 +0100

    setup test for reset store, problem is we can not check private var store, need to think about this one

--- src/main/webapp/resources/app/js/study/studyServiceSpec.js
+++ src/main/webapp/resources/app/js/study/studyServiceSpec.js
@@ -2,29 +2,26 @@
 define(['angular', 'angular-mocks'], function() {
   describe('dataset service', function() {
 
-    var uUIDService;
+    var uUIDService, rdfStoreService;
 
     beforeEach(module('trialverse.study'));
 
-    beforeEach(function() {
-
-      uUIDService = jasmine.createSpyObj('UUIDService', ['generate']);
-
-      module('trialverse', function($provide) {
-        $provide.value('UUIDService', uUIDService);
-      });
-    });
-
     describe('createEmptyStudy', function() {
 
-      var studyService;
-      var q;
-      var scope;
+      var studyService, q;
+
+      beforeEach(function() {
+
+        uUIDService = jasmine.createSpyObj('UUIDService', ['generate']);
+
+        module('trialverse', function($provide) {
+          $provide.value('UUIDService', uUIDService);
+        });
+      });
 
       beforeEach(inject(function($rootScope, $q, StudyService) {
         studyService = StudyService;
         q = $q;
-        scope = $rootScope;
       }));
 
       it('should be defined', function() {
@@ -44,6 +41,42 @@ define(['angular', 'angular-mocks'], function() {
 
       });
 
+    });
+
+    describe('resetStore', function() {
+
+      var studyService, q;
+      beforeEach(function() {
+
+        uUIDService = jasmine.createSpyObj('UUIDService', ['generate']);
+        rdfStoreService = jasmine.createSpyObj('RdfStoreService', ['create']);
+
+        rdfStoreService.create.and.callFake(function(callback) {
+          callback({
+            mock: 'store',
+            load: function(arg1, arg2, callback){
+              callback(true, {mock: 'result'});
+            }
+          });
+        });
+
+        module('trialverse', function($provide) {
+          $provide.value('UUIDService', uUIDService);
+          $provide.value('RdfStoreService', rdfStoreService);
+        });
+      });
+
+      beforeEach(inject(function($rootScope, $q, StudyService) {
+        studyService = StudyService;
+        q = $q;
+        studyService.loadStore({
+          mock: 'data'
+        });
+      }));
+
+      it('should empty the store and reset the storeDefer', function() {
+        studyService.resetStore();
+      });
     });
   });
 });
