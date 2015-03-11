49ec74e47ed84858394ccef26b0b79be9a327678
Daan Reid <osmosisch@gmail.com>
2015-03-11 16:15:18 +0100

    place study on scope

--- src/main/webapp/resources/app/js/concept/conceptController.js
+++ src/main/webapp/resources/app/js/concept/conceptController.js
@@ -5,14 +5,33 @@ define([],
     var ConceptController = function($scope, $stateParams, ConceptService, ConceptResource) {
       var datasetUri = 'http://trials.drugis/org/datasets/' + $stateParams.datasetUUID;
       $scope.concepts = {};
-      ConceptResource.get($stateParams).$promise.then(function(conceptsTurtle) {
-        ConceptService.loadStore(conceptsTurtle.data).then(function() {
-          ConceptService.queryItems(datasetUri).then(function(conceptsJson) {
-            $scope.concepts = conceptsJson;
+
+      function reloadConceptsModel() {
+        ConceptResource.get($stateParams).$promise.then(function(conceptsTurtle) {
+          ConceptService.loadStore(conceptsTurtle.data).then(function() {
+            ConceptService.queryItems(datasetUri).then(function(conceptsJson) {
+              $scope.concepts = conceptsJson;
+            });
           });
         });
-      });
+      }
 
+      function reloadDatasetModel() {
+        DatasetResource.get($stateParams, function(response) {
+          DatasetService.reset();
+          DatasetService.loadStore(response.data).then(function() {
+            DatasetService.queryDataset().then(function(queryResult) {
+              $scope.dataset = queryResult[0];
+              $scope.dataset.uuid = $stateParams.datasetUUID;
+            });
+          });
+        });
+      }
+
+      // onload
+      reloadDatasetModel();
+      reloadConceptsModel();
+      
     };
     return dependencies.concat(ConceptController);
   });
