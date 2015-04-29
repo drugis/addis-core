cbd3fff60cd900465ad2c11bcaa5779d5e2eb187
Daan Reid <osmosisch@gmail.com>
2015-04-29 16:35:59 +0200

    Fix pubmed test

--- addis-rdf/test/org/drugis/addis/rdf/pubmed_test.clj
+++ addis-rdf/test/org/drugis/addis/rdf/pubmed_test.clj
@@ -1,5 +1,6 @@
 (ns org.drugis.addis.rdf.pubmed-test
   (:require [riveted.core :as vtd]
+            [clojure.pprint :as pprint]
             [org.drugis.addis.rdf.trig :as trig]
             [clojure.java.io :refer [as-file]])
   (:use clojure.test)
@@ -34,18 +35,24 @@
     (is (= (:year hansen) (journal-year xml)))))
 
 (defn println* [x]
-  (println x)
+  (pprint/pprint x)
   x)
 
 (deftest test-rdf
   (let [prefixes {:dc "http://purl.org/dc/elements/1.1/" :prism "http://prismstandard.org/namespaces/1.2/basic/" :foaf "http://xmlns.com/foaf/0.1/" :rdf "http://www.w3.org/1999/02/22-rdf-syntax-ns#"}
         rdf (pubmed-rdf (vtd/first-child (vtd/navigator (slurp (as-file "16172440.xml")))))]
+    (println (clojure-version))
     (println rdf)
     (println (trig/write-ttl prefixes [rdf]))
     (is (=
          (trig/spo (pmid-uri (:pmid hansen))
+                   [(trig/iri :dc "identifier") (trig/iri "info:pmid/16172440")]
                    [(trig/iri :dc "title") (:title hansen)]
                    [(trig/iri :dc "creator") (trig/coll (map #(trig/_po [(trig/iri :rdf "type") (trig/iri :foaf "Person")] [(trig/iri :foaf "name") %]) (:authors hansen)))]
                    [(trig/iri :dc "date") (:year hansen)]
-                   [(trig/iri :prism "publicationName") (:journal hansen)])
+                   [(trig/iri :prism "publicationName") (:journal hansen)]
+                   [(trig/iri :prism "issn") (:issn hansen)]
+                   [(trig/iri :prism "volume") (:volume hansen)]
+                   [(trig/iri :prism "number") (:number hansen)]
+                   [(trig/iri :prism "startingPage") (:pages hansen)])
          rdf))))
