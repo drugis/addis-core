439b70d62d85e69d30e809f0a49b0c16fdc87327
Connor Stroomberg <c.stroomberg@gmail.com>
2015-05-22 15:12:12 +0200

    wait for element

--- src/test/integration/loginTest.js
+++ src/test/integration/loginTest.js
@@ -5,9 +5,10 @@ var login = require('./util/login.js');
 module.exports = {
   "Addis login test" : function (browser) {
     login(browser, 'https://addis-test.drugis.org')
-      .waitForElementVisible('body', 5000)
-      .pause(2000)
+      .waitForElementVisible('body', 15000)
+      .pause(5000)
       .assert.containsText('h2', 'Projects')
+      .pause(5000)
       .end();
   }
 };
