2278086ad8d74428a2922bc4c6d46e6504774bde
Connor Stroomberg <c.stroomberg@gmail.com>
2015-05-22 15:54:24 +0200

    dump page source to console

--- src/test/integration/loginTest.js
+++ src/test/integration/loginTest.js
@@ -7,6 +7,10 @@ module.exports = {
     login(browser, 'https://addis-test.drugis.org')
       .waitForElementVisible('body', 15000)
       .pause(5000)
+      .source(function (result){
+              // Source will be stored in result.value
+              console.log(result.value);
+          })
       .assert.containsText('h2', 'Projects')
       .pause(5000)
       .end();
