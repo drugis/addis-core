dc4a3eb67d60920469de1de8c437654fdd53408d
Connor Stroomberg <c.stroomberg@gmail.com>
2015-05-22 15:30:35 +0200

    check text on google page

--- src/test/integration/util/login.js
+++ src/test/integration/util/login.js
@@ -3,6 +3,8 @@ module.exports = function(browser, url){
       .url(url)
       .waitForElementVisible('body', 50000)
       .click('button[type="submit"]')
+      .waitForElementVisible('body', 50000)
+      .assert.containsText('h2', 'Sign in with your Google Account')
       .pause(1000)
       .setValue('input[type=email]', ' addistestuser1@gmail.com')
       .setValue('input[type=password]', 'addistestuser123')
