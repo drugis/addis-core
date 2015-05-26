023c82d27ab8cc45da5251eff0258c52532eccc7
Connor Stroomberg <c.stroomberg@gmail.com>
2015-05-26 10:13:29 +0200

    setup for project test in pages style

---
+++ src/test/integration/pages/projects.js
@@ -0,0 +1,26 @@
+function ProjectsPage(browser) {
+  this.browser = browser;
+}
+
+var LOCATORS = {
+  body: 'body'
+};
+
+ProjectsPage.prototype = {
+  waitForPageToLoad: function() {
+    this.browser.waitForElementVisible(LOCATORS['body'], 50000);
+  },
+  end: function() {
+    this.browser.end();
+  },
+  createProject: function(callback) {
+    this.browser
+      .click('#create-project-btn')
+      .assert.elementPresent('table')
+      .useXpath() // every selector now must be xpath
+      .click("//tr[td='EDARBI']/td/button")
+      .useCss(); // we're back to CSS now
+  }
+};
+
+module.exports = ProjectsPage;
