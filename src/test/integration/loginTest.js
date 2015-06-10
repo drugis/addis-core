0be82910c4e56d541f84fd57cae6f78126f86ed5
Connor Stroomberg <c.stroomberg@gmail.com>
2015-06-10 16:32:08 +0200

    update test to wait for angular

--- src/test/integration/loginTest.js
+++ src/test/integration/loginTest.js
@@ -5,14 +5,14 @@ var login = require('./util/login.js');
 module.exports = {
   "Addis login test" : function (browser) {
     login(browser, 'https://addis-test.drugis.org')
-      .waitForElementVisible('body', 15000)
-      .pause(5000)
+      .waitForElementVisible('h2.nested-view-header', 50000)
+      .pause(1000)
       .source(function (result){
               // Source will be stored in result.value
               console.log(result.value);
           })
-      .assert.containsText('h2', 'Projects')
-      .pause(5000)
+      .assert.containsText('h2.nested-view-header', 'Projects')
+      .pause(1000)
       .end();
   }
 };
