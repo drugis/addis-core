0eb0a1e26a1650076fc3627b68000e06157b784b
Connor Stroomberg <c.stroomberg@gmail.com>
2015-06-19 16:57:34 +0200

    use networkplot directive

--- src/main/webapp/resources/app/js/controllers/networkMetaAnalysisController.js
+++ src/main/webapp/resources/app/js/controllers/networkMetaAnalysisController.js
@@ -6,6 +6,7 @@ define(['d3'], function(d3) {
 
   var NetworkMetaAnalysisController = function($scope, $q, $state, $stateParams, OutcomeResource,
     InterventionResource, TrialverseTrialDataResource, NetworkMetaAnalysisService, ModelResource) {
+    $scope.networkGraph = {};
     $scope.isNetworkDisconnected = true;
     $scope.analysis = $scope.$parent.analysis;
     $scope.project = $scope.$parent.project;
@@ -52,113 +53,6 @@ define(['d3'], function(d3) {
       return memo;
     }
 
-    function drawEdge(enter, fromId, toId, width, circleData) {
-      enter.append('line')
-        .attr('x1', circleData[fromId].cx)
-        .attr('y1', circleData[fromId].cy)
-        .attr('x2', circleData[toId].cx)
-        .attr('y2', circleData[toId].cy)
-        .attr('stroke', 'black')
-        .attr('stroke-width', width);
-    }
-
-    function tanh(x) {
-      var y = Math.exp(2 * x);
-      return (y - 1) / (y + 1);
-    }
-
-    function drawNetwork(network) {
-      var parent = angular.element('#network-graph').parent();
-      var n = network.interventions.length;
-      var angle = 2.0 * Math.PI / n;
-      var originX = parent.width() / 2;
-      var originY = parent.width() / 2;
-      var margin = 200;
-      var radius = originY - margin / 2;
-      var circleMaxSize = 30;
-      var circleMinSize = 5;
-      d3.select('#network-graph').selectAll('g').remove();
-      d3.select('#network-graph').selectAll('line').remove();
-      var svg = d3.select('#network-graph').select('svg')
-        .attr('width', parent.width())
-        .attr('height', parent.width());
-
-
-      var circleData = [];
-
-      _.each(network.interventions, function(intervention, i) {
-        var circleDatum = {
-          id: intervention.name,
-          r: circleMinSize + ((circleMaxSize - circleMinSize) * tanh(intervention.sampleSize / 10000)),
-          cx: originX - radius * Math.cos(angle * i),
-          cy: originY + radius * Math.sin(angle * i)
-        };
-        circleData[intervention.name] = circleDatum;
-        circleData.push(circleDatum);
-      });
-
-      _.each(network.edges, function(edge) {
-        drawEdge(svg, edge.from.name, edge.to.name, edge.numberOfStudies, circleData);
-      });
-
-      var enter = svg.selectAll('g')
-        .data(circleData)
-        .enter()
-        .append('g')
-        .attr('transform', function(d) {
-          return 'translate(' + d.cx + ',' + d.cy + ')';
-        });
-
-      enter.append('circle')
-        .style('fill', 'grey')
-        .attr('r', function(d) {
-          return d.r;
-        });
-
-      var labelMargin = 5;
-      var nearCenterMargin = 20;
-
-      function nearCenter(d) {
-        var delta = d.cx - originX;
-        return delta < -nearCenterMargin ? -1 : (delta > nearCenterMargin ? 1 : 0);
-      }
-
-      var cos45 = Math.sqrt(2) * 0.5;
-      enter.append('text')
-        .attr('dx', function(d) {
-          var offset = cos45 * d.r + labelMargin;
-          return nearCenter(d) * offset;
-        })
-        .attr('dy', function(d) {
-          var offset = (nearCenter(d) === 0 ? d.r : cos45 * d.r) + labelMargin;
-          return (d.cy >= originY ? offset : -offset);
-        })
-        .attr('text-anchor', function(d) {
-          switch (nearCenter(d)) {
-            case -1:
-              return 'end';
-            case 0:
-              return 'middle';
-            case 1:
-              return 'start';
-          }
-        })
-        .attr('dominant-baseline', function(d) {
-          if (nearCenter(d) !== 0) {
-            return 'central';
-          }
-          if (d.cy - originY < 0) {
-            return 'alphabetic';
-          } // text-after-edge doesn't seem to work in Chrome
-          return 'text-before-edge';
-        })
-        .style('font-family', 'Droid Sans')
-        .style('font-size', 16)
-        .text(function(d) {
-          return d.id;
-        });
-    }
-
     function getIncludedInterventions(interventions) {
       return _.filter(interventions, function(intervention) {
         return intervention.isIncluded;
@@ -167,9 +61,8 @@ define(['d3'], function(d3) {
 
     function updateNetwork() {
       var includedInterventions = getIncludedInterventions($scope.interventions);
-      var network = NetworkMetaAnalysisService.transformTrialDataToNetwork($scope.trialverseData, includedInterventions, $scope.analysis.excludedArms);
-      drawNetwork(network);
-      $scope.isNetworkDisconnected = NetworkMetaAnalysisService.isNetworkDisconnected(network);
+      $scope.networkGraph.network = NetworkMetaAnalysisService.transformTrialDataToNetwork($scope.trialverseData, includedInterventions, $scope.analysis.excludedArms);
+      $scope.isNetworkDisconnected = NetworkMetaAnalysisService.isNetworkDisconnected($scope.networkGraph.network);
     }
 
     function reloadModel() {
