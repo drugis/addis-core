6d0b8da4de409ffb0961be651b571c67b988527d
Connor Stroomberg <c.stroomberg@gmail.com>
2015-07-02 09:42:32 +0200

    add dep on gemtc service to addis services

--- src/main/webapp/resources/app/js/services.js
+++ src/main/webapp/resources/app/js/services.js
@@ -1,7 +1,7 @@
 'use strict';
 define(function(require) {
   var angular = require('angular');
-  return angular.module('addis.services', [])
+  return angular.module('addis.services', ['gemtc.services'])
     .factory('Select2UtilService', require('services/select2UtilService'))
     .factory('SingleStudyBenefitRiskAnalysisService', require('services/singleStudyBenefitRiskAnalysisService'))
     .factory('NetworkMetaAnalysisService', require('services/networkMetaAnalysisService'))
