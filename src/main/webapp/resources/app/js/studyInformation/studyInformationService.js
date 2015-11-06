6e25cf42ec8c777d5dd81f5c2bf1c4a4af41e1a1
Daan Reid <osmosisch@gmail.com>
2015-11-06 17:17:52 +0100

    Cleanup & refactor studyInformation

--- src/main/webapp/resources/app/js/studyInformation/studyInformationService.js
+++ src/main/webapp/resources/app/js/studyInformation/studyInformationService.js
@@ -1,54 +1,55 @@
 'use strict';
 define([],
   function() {
-    var dependencies = ['$q', 'StudyService', 'SparqlResource', 'UUIDService',
+    var dependencies = ['$q', 'StudyService', 'UUIDService',
      'BLINDING_OPTIONS', 'GROUP_ALLOCATION_OPTIONS', 'STATUS_OPTIONS', 'SanitizeService'];
-    var StudyInformationService = function($q, StudyService, SparqlResource,
-     UUIDService, BLINDING_OPTIONS, GROUP_ALLOCATION_OPTIONS, STATUS_OPTIONS, SanitizeService) {
-
-      var studyInformationQuery = SparqlResource.get('queryStudyInformation.sparql');
-      var editBlindingTemplate = SparqlResource.get('editBlinding.sparql');
-      var deleteBlindingTemplate = SparqlResource.get('deleteBlinding.sparql');
-      var editGroupAllocationTemplate = SparqlResource.get('editGroupAllocation.sparql');
-      var deleteGroupAllocationTemplate = SparqlResource.get('deleteGroupAllocation.sparql');
-      var editStatusTemplate = SparqlResource.get('editStatus.sparql');
-      var deleteStatusTemplate = SparqlResource.get('deleteStatus.sparql');
-      var editNumberOfCentersTemplate = SparqlResource.get('editNumberOfCenters.sparql');
-      var deleteNumberOfCentersTemplate = SparqlResource.get('deleteNumberOfCenters.sparql');
-      var editObjectiveTemplate = SparqlResource.get('editObjective.sparql');
-      var deleteObjectiveTemplate = SparqlResource.get('deleteObjective.sparql');
+    var StudyInformationService = function($q, StudyService, UUIDService,
+      BLINDING_OPTIONS, GROUP_ALLOCATION_OPTIONS, STATUS_OPTIONS, SanitizeService) {
 
       function queryItems() {
-        return studyInformationQuery.then(function(query) {
-          return StudyService.doNonModifyingQuery(query).then(function(result) {
-            var transformedResult = {
-              blinding: {},
-              groupAllocation: {},
-              status: {}
-            };
-            if (result.length > 0) {
-              transformedResult.blinding.uri = result[0].blindingUri;
-              transformedResult.groupAllocation.uri = result[0].groupAllocationUri;
-              transformedResult.status.uri = result[0].statusUri;
-              transformedResult.numberOfCenters = parseInt(result[0].numberOfCenters);
-              transformedResult.objective = result[0].objective;
-            }
-            return [transformedResult];
-          });
+        return StudyService.getStudy().then(function(study) {
+          return [{
+            blinding: study.has_blinding,
+            groupAllocation: study.has_allocation,
+            status: study.status,
+            numberOfCenters: study.has_number_of_centers,
+            objective: study.has_objective
+          }];
         });
       }
 
+
+      // function queryItems() {
+      //   return studyInformationQuery.then(function(query) {
+      //     return StudyService.doNonModifyingQuery(query).then(function(result) {
+      //       var transformedResult = {
+      //         blinding: {},
+      //         groupAllocation: {},
+      //         status: {}
+      //       };
+      //       if (result.length > 0) {
+      //         transformedResult.blinding.uri = result[0].blindingUri;
+      //         transformedResult.groupAllocation.uri = result[0].groupAllocationUri;
+      //         transformedResult.status.uri = result[0].statusUri;
+      //         transformedResult.numberOfCenters = parseInt(result[0].numberOfCenters);
+      //         transformedResult.objective = result[0].objective;
+      //       }
+      //       return [transformedResult];
+      //     });
+      //   });
+      // }
+
       function editProperty(item, editCondition, editTemplate, deleteTemplate) {
-        if (editCondition) {
-          return editTemplate.then(function(template) {
-            var query = fillTemplate(template, item);
-            return StudyService.doModifyingQuery(query);
-          });
-        } else {
-          return deleteTemplate.then(function(template) {
-            return StudyService.doModifyingQuery(template);
-          });
-        }
+        // if (editCondition) {
+        //   return editTemplate.then(function(template) {
+        //     var query = fillTemplate(template, item);
+        //     return StudyService.doModifyingQuery(query);
+        //   });
+        // } else {
+        //   return deleteTemplate.then(function(template) {
+        //     return StudyService.doModifyingQuery(template);
+        //   });
+        // }
       }
 
       function editSelectItem(item, selectedProperty, options, editTemplate, deleteTemplate) {
