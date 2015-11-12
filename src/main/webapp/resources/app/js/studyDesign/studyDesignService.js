eee6e5894fefca2838a21e9c22716c2c26cfcc04
Connor Stroomberg <c.stroomberg@gmail.com>
2015-11-12 10:39:51 +0100

    refactor studyDesignService, queryItems done

--- src/main/webapp/resources/app/js/studyDesign/studyDesignService.js
+++ src/main/webapp/resources/app/js/studyDesign/studyDesignService.js
@@ -4,37 +4,53 @@ define([],
     var dependencies = ['$q', 'StudyService', 'SparqlResource', 'UUIDService'];
     var StudyDesignService = function($q, StudyService, SparqlResource, UUIDService) {
 
-      var queryActivityCoordinatesTemplate = SparqlResource.get('queryActivityCoordinates.sparql');
-      var setActivityCoordinatesTemplate = SparqlResource.get('setActivityCoordinates.sparql');
-      var cleanupCoordinatesTemplate = SparqlResource.get('cleanupCoordinates.sparql');
+      //var queryActivityCoordinatesTemplate = SparqlResource.get('queryActivityCoordinates.sparql');
+      //var setActivityCoordinatesTemplate = SparqlResource.get('setActivityCoordinates.sparql');
+      //var cleanupCoordinatesTemplate = SparqlResource.get('cleanupCoordinates.sparql');
+
+      function createCoordinate(activityUri, activityApplication) {
+        return {
+          activityUri: activityUri,
+          epochUri: activityApplication.applied_in_epoch,
+          armUri: activityApplication.applied_to_arm
+        };
+      }
+
+      function flattenActivity(acumulator, activity) {
+        return acumulator
+          .concat(
+            activity
+            .has_activity_application
+            .map(createCoordinate.bind(this, activity['@id']))
+          );
+      }
 
       function queryItems() {
-        return queryActivityCoordinatesTemplate.then(function(template){
-          var query = fillInTemplate(template, {});
-          return StudyService.doNonModifyingQuery(query);
+        return StudyService.getStudy().then(function(study) {
+          return study.has_activity.reduce(flattenActivity, []);
         });
       }
 
       function setActivityCoordinates(coordinates) {
-        return setActivityCoordinatesTemplate.then(function(template){
-          var query = fillInTemplate(template, coordinates);
-          return StudyService.doModifyingQuery(query);
-        });
+        // return setActivityCoordinatesTemplate.then(function(template){
+        //   var query = fillInTemplate(template, coordinates);
+        //   return StudyService.doModifyingQuery(query);
+        // });
       }
 
       function cleanupCoordinates() {
-        return cleanupCoordinatesTemplate.then(function(template) {
-          var query = fillInTemplate(template, {});
-          return StudyService.doModifyingQuery(query);
-        });
+        // return cleanupCoordinatesTemplate.then(function(template) {
+        //   var query = fillInTemplate(template, {});
+        //   return StudyService.doModifyingQuery(query);
+        // });
       }
 
-      function fillInTemplate(template, coordinates) {
-        return template
-            .replace(/\$epochUri/g, coordinates.epochUri)
-            .replace(/\$armUri/g, coordinates.armUri)
-            .replace(/\$activityUri/g, coordinates.activityUri);
-      }
+      // function fillInTemplate(template, coordinates) {
+      //   return template
+      //       .replace(/\$epochUri/g, coordinates.epochUri)
+      //       .replace(/\$armUri/g, coordinates.armUri)
+      //       .replace(/\$activityUri/g, coordinates.activityUri);
+      // }
 
       return {
         queryItems: queryItems,
