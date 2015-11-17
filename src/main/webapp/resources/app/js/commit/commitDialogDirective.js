e663e226c15095ef065c5960a310f4a7ca5d62f5
Connor Stroomberg <c.stroomberg@gmail.com>
2015-11-17 09:07:33 +0100

    seperate function to get graph and context for use in commit

--- src/main/webapp/resources/app/js/commit/commitDialogDirective.js
+++ src/main/webapp/resources/app/js/commit/commitDialogDirective.js
@@ -16,25 +16,8 @@ define([], function() {
       link: function(scope) {
         var ItemService = $injector.get(scope.itemServiceName);
 
-        // scope.commitChanges = function(commitTitle, commitDescription) {
-        //   ItemService.getGraph().then(function(graph) {
-        //     GraphResource.put({
-        //       userUid: scope.userUid,
-        //       datasetUUID: scope.datasetUuid,
-        //       graphUuid: scope.graphUuid,
-        //       commitTitle: commitTitle,
-        //       commitDescription: commitDescription
-        //     }, graph.data, function(value, responseHeaders) {
-        //       var newVersion = responseHeaders('X-EventSource-Version');
-        //       newVersion = newVersion.split('/')[4];
-        //       scope.changesCommited(newVersion);
-        //     });
-        //   });
-        // };
-
-
         scope.commitChanges = function(commitTitle, commitDescription) {
-          ItemService.getJsonGraph().then(function(graph) {
+          ItemService.getGraphAndContext().then(function(graph) {
             GraphResource.putJson({
               userUid: scope.userUid,
               datasetUUID: scope.datasetUuid,
