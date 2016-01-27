0adb2589fc0a6ddc0e783e13a54597f8c7b6779c
Daan Reid <osmosisch@gmail.com>
2016-01-27 11:21:40 +0100

    The tyranny of jshint

--- src/main/webapp/resources/app/js/util/transformConceptJsonLd.js
+++ src/main/webapp/resources/app/js/util/transformConceptJsonLd.js
@@ -1,16 +1,17 @@
 'use strict';
-define(['lodash'], function(_) {
+define([], function() {
   // Work on object as object is both cashe and promise
   function transformConceptJson(conceptJson) {
     // fix the 1-item case
+    var context = conceptJson['@context'];
+
     if (!conceptJson['@graph']) {
-      var context = conceptJson['@context'];
       delete conceptJson['@context'];
       conceptJson['@graph'] = [{
         '@id': conceptJson['@id'],
         '@type': conceptJson['@type'],
-        'label': conceptJson['label'],
-        'comment': conceptJson['comment'],
+        'label': conceptJson.label,
+        'comment': conceptJson.comment,
       }];
       delete conceptJson['@id'];
       delete conceptJson['@type'];
@@ -18,19 +19,17 @@ define(['lodash'], function(_) {
       delete conceptJson.comment;
       conceptJson['@context'] = context;
     }
-    // add to the context
-    var context = conceptJson['@context'];
-    context['ontology'] = 'http://trials.drugis.org/ontology#';
-    context['comment'] = 'http://www.w3.org/2000/01/rdf-schema#comment';
-    context['label'] = 'http://www.w3.org/2000/01/rdf-schema#label';
+    context.ontology = 'http://trials.drugis.org/ontology#';
+    context.comment = 'http://www.w3.org/2000/01/rdf-schema#comment';
+    context.label = 'http://www.w3.org/2000/01/rdf-schema#label';
     // standardize type URIs
     conceptJson['@graph'] = conceptJson['@graph'].map(function(item) {
-      if(item['@type']) {
-        item['@type'] = item['@type'].replace(context['ontology'], 'ontology:');
+      if (item['@type']) {
+        item['@type'] = item['@type'].replace(context.ontology, 'ontology:');
       }
       return item;
     });
     return conceptJson;
   }
   return transformConceptJson;
-})
+});
