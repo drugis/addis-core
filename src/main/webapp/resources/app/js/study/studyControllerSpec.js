90794c1b81f82abb86993ea2b8227fa99d7284c1
Connor Stroomberg <c.stroomberg@gmail.com>
2016-01-27 13:44:35 +0100

    fix studyController test, hard stuff is tested but still needs some more tests

--- src/main/webapp/resources/app/js/study/studyControllerSpec.js
+++ src/main/webapp/resources/app/js/study/studyControllerSpec.js
@@ -1,8 +1,8 @@
 'use strict';
-define(['angular', 'angular-mocks'], function() {
+define(['angular-mocks'], function(angularMocks) {
   describe('the study controller', function() {
 
-    var scope, httpBackend,
+    var scope,
       userUid = 'userUid',
       datasetUUID = 'datasetUUID',
       studyGraphUuid = 'studyGraphUuid',
@@ -16,76 +16,97 @@ define(['angular', 'angular-mocks'], function() {
       anchorScrollMock = jasmine.createSpy('anchorScroll'),
       locationMock = jasmine.createSpyObj('location', ['hash']),
       modalMock = jasmine.createSpyObj('modal', ['open']),
-      studyServiceMock = jasmine.createSpyObj('StudyService',
-       ['reset', 'queryArmData', 'loadStore', 'queryStudyData', 'getGraph', 'studySaved', 'loadJson']),
+      studyService = jasmine.createSpyObj('StudyService', ['reset', 'loadJson', 'getStudy',
+      'studySaved', 'isStudyModified' ]),
       resultsServiceMock = jasmine.createSpyObj('ResultsService', ['cleanupMeasurements']),
       studyDesignServiceMock = jasmine.createSpyObj('StudyDesignService', ['cleanupCoordinates']),
-      loadStoreDeferred,
+      graphResource = jasmine.createSpyObj('GraphResource', ['getJson']),
+      versionedGraphResource = jasmine.createSpyObj('VersionedGraphResource', ['getJson']),
       queryStudyDataDeferred,
       queryArmDataDeferred,
       getGraphDeferred,
+      loadJsonDefferd,
+      getJsonResultDefferd,
       getJsonDeferred;
 
 
     beforeEach(module('trialverse.study'));
 
-    beforeEach(inject(function($rootScope, $q, $controller, $httpBackend, VersionedGraphResource) {
+    beforeEach(angularMocks.inject(function($rootScope, $q) {
 
       scope = $rootScope;
-      httpBackend = $httpBackend;
+      spyOn(scope, '$broadcast');
 
-      httpBackend.expectGET('/users/' + userUid + '/datasets/' + datasetUUID + '/versions/' + versionUuid + '/graphs/' + studyGraphUuid).respond('study');
-
-      loadStoreDeferred = $q.defer();
       queryStudyDataDeferred = $q.defer();
       queryArmDataDeferred = $q.defer();
       getGraphDeferred = $q.defer();
       getJsonDeferred = $q.defer();
+      loadJsonDefferd = $q.defer();
+      getJsonResultDefferd = $q.defer();
 
-      studyServiceMock.loadStore.and.returnValue(loadStoreDeferred.promise);
-      studyServiceMock.queryStudyData.and.returnValue(queryStudyDataDeferred.promise);
-      studyServiceMock.queryArmData.and.returnValue(queryArmDataDeferred.promise);
-      studyServiceMock.getGraph.and.returnValue(getGraphDeferred.promise);
-      
-      spyOn(VersionedGraphResource, "getJson").and.returnValue(getJsonDeferred);
-
-
-      $controller('StudyController', {
-        $scope: scope,
-        $stateParams: mockStateParams,
-        VersionedGraphResource: VersionedGraphResource,
-        $location: locationMock,
-        $anchorScroll: anchorScrollMock,
-        $modal: modalMock,
-        $window: {
-          bind: 'mockBind',
-          innerHeight: 'innerHeightMock'
-        },
-        StudyService: studyServiceMock,
-        ResultsService: resultsServiceMock,
-        StudyDesignService: studyDesignServiceMock,
-      });
-
-
+      studyService.loadJson.and.returnValue(getJsonDeferred.promise);
     }));
 
-    describe('on load', function() {
+    describe('on load of versionedView', function() {
 
-      it('should place study on the scope', function() {
-        var
-          studyQueryResult = 'studyQueryResult',
-          armQueryResult = 'armQueryResult';
+      var jsonResultDefer;
+      var getJsonResult;
+      var getStudyResult;
 
-        expect(scope.study).toBeDefined();
-        httpBackend.flush();
-        expect(studyServiceMock.loadStore).toHaveBeenCalled();
-        loadStoreDeferred.resolve(3);
-        scope.$digest();
-        expect(studyServiceMock.queryStudyData).toHaveBeenCalled();
-        queryStudyDataDeferred.resolve(studyQueryResult);
-        scope.$digest();
-        expect(scope.study).toBe(studyQueryResult);
+      beforeEach(angularMocks.inject(function($q, $controller) {
+
+        jsonResultDefer = $q.defer();
+        getJsonResult = {
+          $promise: jsonResultDefer.promise,
+          mockJson: '123'
+        };
+        versionedGraphResource.getJson.and.returnValue(getJsonResult);
+
+        getStudyResult = $q.defer();
+        studyService.getStudy.and.returnValue(getStudyResult.promise);
+
+        $controller('StudyController', {
+          $scope: scope,
+          $stateParams: mockStateParams,
+          VersionedGraphResource: versionedGraphResource,
+          GraphResource: graphResource,
+          $location: locationMock,
+          $anchorScroll: anchorScrollMock,
+          $modal: modalMock,
+          $window: {
+            bind: 'mockBind',
+            innerHeight: 'innerHeightMock'
+          },
+          StudyService: studyService,
+          ResultsService: resultsServiceMock,
+          StudyDesignService: studyDesignServiceMock,
+        });
+      }));
+
+      it('should reset the study service', function() {
+        expect(studyService.reset).toHaveBeenCalled();
       });
+
+      it('should load the study data', function() {
+        expect(versionedGraphResource.getJson).toHaveBeenCalled();
+      });
+
+      it('should place loaded data into fontend cache', function() {
+        expect(studyService.loadJson).toHaveBeenCalledWith(getJsonResult.$promise);
+      });
+
+      it('should use the loaded data to fill the view and alert the subviews', function() {
+        jsonResultDefer.resolve({bla: 'bla'});
+        scope.$digest();
+        expect(studyService.getStudy).toHaveBeenCalled();
+        getStudyResult.resolve({study: 'studyjson'});
+        scope.$digest();
+        expect(studyService.studySaved).toHaveBeenCalled();
+        expect(scope.$broadcast).toHaveBeenCalledWith('refreshStudyDesign');
+        expect(scope.$broadcast).toHaveBeenCalledWith('refreshResults');
+      });
+
+
 
     });
 
