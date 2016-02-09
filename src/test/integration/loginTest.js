8ff85dede46a8fedf58764cff986de8270088e20
Connor Stroomberg <c.stroomberg@gmail.com>
2016-02-09 15:05:51 +0100

    user env var to pass test url

--- src/test/integration/loginTest.js
+++ src/test/integration/loginTest.js
@@ -1,10 +1,9 @@
 var login = require('./util/login.js');
-
-// test the login
+var testUrl = process.env.ADDIS_TEST_URL ? process.env.ADDIS_TEST_URL : 'https://addis-test.drugis.org';
 
 module.exports = {
   "Addis login test" : function (browser) {
-    login(browser, 'https://addis-test.drugis.org')
+    login(browser, testUrl)
       .waitForElementVisible('#user-view-user-name', 50000)
       .pause(1000)
       .assert.containsText('#user-view-user-name', 'Ulrika Tester')
