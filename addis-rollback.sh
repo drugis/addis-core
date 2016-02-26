a270e75404a60dbce488c0df7c58f3d36edadd2e
Connor Stroomberg <c.stroomberg@gmail.com>
2016-02-26 10:52:02 +0100

    rollback last changeset on local addis db using mvn

---
+++ addis-rollback.sh
@@ -0,0 +1,1 @@
+mvn liquibase:rollback -Dliquibase.rollbackCount=1 -Dliquibase.changeLogFile=database.sql -Dliquibase.driver=org.postgresql.Driver -Dliquibase.url=jdbc:postgresql://localhost/addiscore -Dliquibase.username=addiscore -Dliquibase.password=develop
