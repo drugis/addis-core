e66ec99d321223ff0841928b9db4c248612abcda
Connor Stroomberg <c.stroomberg@gmail.com>
2016-03-24 15:57:36 +0100

    integration test setup

--- src/test/integration/util/login.js
+++ src/test/integration/util/login.js
@@ -1,5 +1,7 @@
+var testUrl = process.env.ADDIS_TEST_URL ? process.env.ADDIS_TEST_URL : 'https://addis-test.drugis.org';
+
 module.exports = function(browser, url){
-    return browser
+    browser
       .url(url)
       .waitForElementVisible('body', 50000)
       .click('button[type="submit"]')
@@ -10,8 +12,10 @@ module.exports = function(browser, url){
       .click('input[type="submit"]')
       .pause(1000)
       .setValue('input[type=password]', 'speciaalvoordejenkins')
-      .click('#signIn')
-      // No need to approve approve access each time
-      // .pause(3000) // wait for submit button to become active (thanks for keeping us safe google)
-      // .click('#submit_approve_access')
+      .click('#signIn');
+      if (testUrl.startsWith('http://localhost')) {
+          browser.pause(3000) // wait for submit button to become active (thanks for keeping us safe google)
+              .click('#submit_approve_access');
+      }
+      return browser;
 };
