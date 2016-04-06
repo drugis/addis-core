043209791b1b8bd02f07a0f5a4fd39961c55a276
Daan Reid <osmosisch@gmail.com>
2016-04-06 09:50:38 +0200

    Persistence WIP

--- src/main/java/org/drugis/addis/interventions/model/TitratedDoseIntervention.java
+++ src/main/java/org/drugis/addis/interventions/model/TitratedDoseIntervention.java
@@ -1,10 +1,32 @@
 package org.drugis.addis.interventions.model;
 
+import javax.persistence.*;
+
 /**
  * Created by daan on 5-4-16.
  */
+@Entity
 public class TitratedDoseIntervention extends AbstractIntervention {
+  @Embedded
+  @AttributeOverrides( {
+          @AttributeOverride(name="lowerBound.lowerBoundType" , column = @Column(name="minLowerBoundType") ),
+          @AttributeOverride(name="lowerBound.lowerBoundUnit" , column = @Column(name="minLowerBoundUnit") ),
+          @AttributeOverride(name="lowerBound.lowerBoundValue", column = @Column(name="minLowerBoundValue") ),
+          @AttributeOverride(name="upperBound.upperBoundType" , column = @Column(name="minUpperBoundType") ),
+          @AttributeOverride(name="upperBound.upperBoundUnit" , column = @Column(name="minUpperBoundUnit") ),
+          @AttributeOverride(name="upperBound.upperBoundValue", column = @Column(name="minUpperBoundValue") )
+  } )
   private DoseConstraint minConstraint;
+
+  @Embedded
+  @AttributeOverrides( {
+          @AttributeOverride(name="lowerBound.lowerBoundType" , column = @Column(name="maxLowerBoundType") ),
+          @AttributeOverride(name="lowerBound.lowerBoundUnit" , column = @Column(name="maxLowerBoundUnit") ),
+          @AttributeOverride(name="lowerBound.lowerBoundValue", column = @Column(name="maxLowerBoundValue") ),
+          @AttributeOverride(name="upperBound.upperBoundType" , column = @Column(name="maxUpperBoundType") ),
+          @AttributeOverride(name="upperBound.upperBoundUnit" , column = @Column(name="maxUpperBoundUnit") ),
+          @AttributeOverride(name="upperBound.upperBoundValue", column = @Column(name="maxUpperBoundValue") )
+  } )
   private DoseConstraint maxConstraint;
 
   public TitratedDoseIntervention() {
