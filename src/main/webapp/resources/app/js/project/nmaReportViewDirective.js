b5222586c9a604eb1b37a4127319bfc3e8eabf68
Connor Stroomberg <c.stroomberg@gmail.com>
2016-04-28 13:51:05 +0200

    WIP fetch the  results for report view

--- src/main/webapp/resources/app/js/project/nmaReportViewDirective.js
+++ src/main/webapp/resources/app/js/project/nmaReportViewDirective.js
@@ -1,7 +1,12 @@
 'use strict';
 define(['lodash'], function(_) {
-  var dependencies = ['EvidenceTableResource', 'NetworkMetaAnalysisService', 'ModelResource', 'AnalysisService'];
-  var nmaReportViewDirective = function(EvidenceTableResource, NetworkMetaAnalysisService, ModelResource, AnalysisService) {
+  var dependencies = ['EvidenceTableResource', 'NetworkMetaAnalysisService',
+    'ModelResource', 'AnalysisService', 'PataviTaskIdResource', 'PataviService',
+    'ProblemResource'
+  ];
+  var nmaReportViewDirective = function(EvidenceTableResource,
+    NetworkMetaAnalysisService, ModelResource, AnalysisService,
+    PataviTaskIdResource, PataviService, ProblemResource) {
     return {
       restrict: 'E',
       templateUrl: 'app/js/project/nmaReportView.html',
@@ -23,11 +28,17 @@ define(['lodash'], function(_) {
           });
 
 
-        ModelResource.query({
+        var modelPromise = ModelResource.query({
           projectId: scope.project.id,
           analysisId: scope.analysis.id
-        }).$promise.then(function(models) {
+        }).$promise;
 
+        scope.problem = ProblemResource.get({
+          analysisId: scope.analysis.id,
+          projectId: scope.project.id
+        });
+
+        modelPromise.then(function(models) {
           scope.primaryModel = _.find(models, function(model) {
             return model.id === scope.analysis.primaryModel;
           });
@@ -35,6 +46,8 @@ define(['lodash'], function(_) {
           scope.otherModels = _.filter(models, function(model) {
             return model.id !== scope.analysis.primaryModel;
           });
+
+          getResults(scope.primaryModel);
         });
 
         scope.modelSettingSummary = function(model) {
@@ -47,23 +60,41 @@ define(['lodash'], function(_) {
           return modelPart + ' ' + typePart + ' on the ' + scalePart + ' scale.';
         };
 
-        function modelTypeString(type) {
-          var typeString;
-          if (type === 'network') {
-            typeString = 'network meta-analysis';
-          } else if (type === 'pair-wise') {
-            typeString = 'pair-wise meta-analysis';
-          } else if (type === 'node-split') {
-            typeString = 'node-splitting analysis';
-          } else if (type === 'regression') {
-            typeString = 'meta-regression';
-          }
-          return typeString;
+        var getResults = function(model) {
+          PataviTaskIdResource
+            .get({projectId: scope.project.id, analysisId: scope.analysis.id, modelId: model.id})
+            .$promise
+            .then(PataviService.run)
+            .then(function(result) {
+              scope.result = result.results;
+            },
+            function(pataviError) {
+              console.error('an error has occurred, error: ' + JSON.stringify(pataviError));
+              scope.$emit('error', {
+                type: 'patavi',
+                message: pataviError.desc
+              });
+            });
+
+      };
+
+      function modelTypeString(type) {
+        var typeString;
+        if (type === 'network') {
+          typeString = 'network meta-analysis';
+        } else if (type === 'pair-wise') {
+          typeString = 'pair-wise meta-analysis';
+        } else if (type === 'node-split') {
+          typeString = 'node-splitting analysis';
+        } else if (type === 'regression') {
+          typeString = 'meta-regression';
         }
-
-
+        return typeString;
       }
-    };
+
+
+    }
   };
-  return dependencies.concat(nmaReportViewDirective);
+};
+return dependencies.concat(nmaReportViewDirective);
 });
