39aa34f3a997c5b2f65069a823a8d02a0f210017
Connor Stroomberg <c.stroomberg@gmail.com>
2016-07-22 15:21:18 +0200

    bug fix , add comma

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
       'karma-chrome-launcher',
       'karma-junit-reporter',
       'karma-jasmine',
-      'karma-phantomjs-launcher'
+      'karma-phantomjs-launcher',
       'karma-requirejs'
     ],
 
