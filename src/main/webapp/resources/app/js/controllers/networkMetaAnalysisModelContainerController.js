d93018dd4f0c92c20ba22dffcf4dc12f8e84bf89
Daan Reid <osmosisch@gmail.com>
2016-08-30 16:35:12 +0200

    Also set editing info for model vie

--- src/main/webapp/resources/app/js/controllers/networkMetaAnalysisModelContainerController.js
+++ src/main/webapp/resources/app/js/controllers/networkMetaAnalysisModelContainerController.js
@@ -1,14 +1,26 @@
 'use strict';
 define([], function() {
-  var dependencies = ['$scope', '$state', 'ProjectResource', 'AnalysisResource'];
+  var dependencies = ['$scope', '$state', 'ProjectResource', 'AnalysisResource', 'UserService'];
 
-  var NetworkMetaAnalysisModelContainerController = function($scope, $state, ProjectResource, AnalysisResource) {
+  var NetworkMetaAnalysisModelContainerController = function($scope, $state, ProjectResource, AnalysisResource, UserService) {
     $scope.project = ProjectResource.get({
       projectId: $state.params.projectId
     });
     $scope.analysis = AnalysisResource.get($state.params);
     $scope.userId = $state.params.userUid;
     $scope.userUid = $state.params.userUid;
+
+    var isUserOwner = false;
+
+    if (UserService.hasLoggedInUser()) {
+      $scope.loginUserId = (UserService.getLoginUser()).id;
+      isUserOwner = UserService.isLoginUserId($scope.project.owner.id);
+    }
+
+    $scope.editMode = {
+      isUserOwner: isUserOwner,
+      disableEditing: !isUserOwner
+    };
   };
 
   return dependencies.concat(NetworkMetaAnalysisModelContainerController);
