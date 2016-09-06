8a69e5c0cbdf62436b99b752c378573c8fb67a71
Daan Reid <osmosisch@gmail.com>
2016-09-06 09:13:19 +0200

    refactor

--- src/main/webapp/resources/app/js/populationCharacteristic/populationCharacteristic.js
+++ src/main/webapp/resources/app/js/populationCharacteristic/populationCharacteristic.js
@@ -9,10 +9,6 @@ define(function(require) {
     'trialverse.outcome'
   ])
 
-  // controllers
-  .controller('CreatePopulationCharacteristicController', require('populationCharacteristic/createPopulationCharacteristicController'))
-  .controller('EditPopulationCharacteristicController', require('populationCharacteristic/editPopulationCharacteristicController'))
-
   //services
   .factory('PopulationCharacteristicService', require('populationCharacteristic/populationCharacteristicService'));
 });
