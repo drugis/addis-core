068ed75336f0b65e654b522e779175a04413c9ee
Joris de Keijser <chinook25@gmail.com>
2016-11-23 16:17:57 +0100

    save button gives feedback

--- src/main/webapp/resources/app/js/project/editReportController.js
+++ src/main/webapp/resources/app/js/project/editReportController.js
@@ -1,11 +1,12 @@
 'use strict';
 define(['angular', 'lodash'],
   function() {
-    var dependencies = ['$scope', '$q', '$stateParams', 'ProjectResource', 'ReportResource'];
-    var EditReportcontroller = function($scope, $q, $stateParams, ProjectResource, ReportResource) {
+    var dependencies = ['$scope', '$q', '$stateParams', 'ProjectResource', 'ReportResource', '$timeout'];
+    var EditReportcontroller = function($scope, $q, $stateParams, ProjectResource, ReportResource, $timeout) {
       $scope.project = ProjectResource.get($stateParams);
       $scope.reportText = '';
-
+      $scope.showSaving = false;
+      $scope.showSaved = false;
       $q.all([$scope.project.$promise, ReportResource.get($stateParams).$promise]).then(function(values) {
         $scope.loading = {
           loaded: true
@@ -18,6 +19,15 @@ define(['angular', 'lodash'],
 
       function saveChanges(newText) {
         ReportResource.put($stateParams, newText);
+        $scope.showSaving = true;
+        $timeout(function() {
+          $scope.showSaving = false;
+          $scope.showSaved = true;
+        }, 1000);
+        $timeout(function() {
+          $scope.showSaved = false;
+        }, 3000);
+
       }
 
     };
