7dc9cc7ae0b15c58070d11045337b6f932a4216a
Joris de Keijser <chinook25@gmail.com>
2016-12-05 12:20:07 +0100

    no custom report means report view shows legacystyle report once again

--- src/main/webapp/resources/app/js/controllers/singleProjectController.js
+++ src/main/webapp/resources/app/js/controllers/singleProjectController.js
@@ -20,7 +20,6 @@ define(['lodash', 'angular'], function(_, angular) {
   var SingleProjectController = function($scope, $q, $state, $stateParams, $location, $modal, ProjectResource, TrialverseResource,
     TrialverseStudyResource, SemanticOutcomeResource, OutcomeResource, SemanticInterventionResource, InterventionResource,
     CovariateOptionsResource, CovariateResource, AnalysisResource, ANALYSIS_TYPES, InterventionService, activeTab, UserService, ReportResource) {
-    //TODO: clean up file as a lot is not necessary anymore.
     $scope.activeTab = activeTab;
 
     $scope.analysesLoaded = false;
@@ -95,6 +94,12 @@ define(['lodash', 'angular'], function(_, angular) {
         });
       });
       $scope.reportText = ReportResource.get($stateParams);
+
+      $scope.reportText.$promise.then(function() {
+        if ($scope.reportText.data.localeCompare('default report text') === 0) {
+          $scope.showLegacyReport = true;
+        }
+      });
     });
 
     function generateInterventionDescriptions(interventions) {
