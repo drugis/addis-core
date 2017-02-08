38aa3ddcb0063acae9af79e209e6ce019fce4f98
chinook25 <chinook25@gmail.com>
2017-02-08 16:34:28 +0100

    relative effect table - Next up: directive!

--- src/main/java/org/drugis/addis/models/repository/impl/ModelRepositoryImpl.java
+++ src/main/java/org/drugis/addis/models/repository/impl/ModelRepositoryImpl.java
@@ -70,7 +70,7 @@ public class ModelRepositoryImpl implements ModelRepository {
   }
 
   @Override
-  public List<Model> findNetworkModelsByProject(Integer projectId) throws SQLException {
+  public List<Model> findModelsByProject(Integer projectId) throws SQLException {
     TypedQuery<Model> query = em.createQuery("SELECT m FROM Model m, NetworkMetaAnalysis a WHERE " +
             "m.analysisId = a.id AND a.projectId = :projectId", Model.class);
     query.setParameter("projectId", projectId);
