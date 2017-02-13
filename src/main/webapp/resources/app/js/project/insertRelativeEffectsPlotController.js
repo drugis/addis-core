8dab9b04477b28baa1fbf5b5011ca1200377ab65
chinook25 <chinook25@gmail.com>
2017-02-13 10:04:52 +0100

    relative effects plot done

--- src/main/webapp/resources/app/js/project/insertRelativeEffectsPlotController.js
+++ src/main/webapp/resources/app/js/project/insertRelativeEffectsPlotController.js
@@ -1,12 +1,13 @@
 'use strict';
 define(['lodash'], function(_) {
   var dependencies = ['$scope', '$stateParams', '$modalInstance', '$q', 'AnalysisResource',
-    'ReportDirectiveService', 'callback', 'ModelResource'
+    'ReportDirectiveService', 'callback', 'ModelResource', 'InterventionResource'
   ];
   var InsertRelativeEffectsTableController = function($scope, $stateParams, $modalInstance, $q, AnalysisResource,
-    ReportDirectiveService, callback, ModelResource) {
+    ReportDirectiveService, callback, ModelResource, InterventionResource) {
     var analysesPromise = AnalysisResource.query($stateParams).$promise;
     var modelsPromise = ModelResource.queryByProject($stateParams).$promise;
+    var interventionPromise = InterventionResource.queryByProject($stateParams).$promise;
 
     $scope.selections = {};
     $scope.isRegressionModel = false;
@@ -18,9 +19,11 @@ define(['lodash'], function(_) {
       loaded: false
     };
 
-    $q.all([analysesPromise, modelsPromise]).then(function(values) {
+    $q.all([analysesPromise, modelsPromise, interventionPromise]).then(function(values) {
       var analyses = values[0];
       var models = values[1];
+      $scope.interventions = values[2];
+
       models = _.chain(models).reject('archived').reject(['modelType.type', 'node-split']).value();
       $scope.analyses = _.chain(analyses)
         .reject('archived')
@@ -42,14 +45,23 @@ define(['lodash'], function(_) {
       if (!$scope.selections.analysis) {
         return;
       }
-      if ($scope.selections.analysis.interventionInclusions) {
-        $scope.selections.baselineTreatment = $scope.selections.analysis.interventionInclusions[0];
-      }
+
       if ($scope.selections.analysis.primaryModel) {
         $scope.selections.model = _.find($scope.selections.analysis.models, ['id', $scope.selections.analysis.primaryModel]);
       } else {
         $scope.selections.model = $scope.selections.analysis.models[0];
       }
+
+      $scope.selections.analysis.interventionsWithName = [];
+      _.forEach($scope.interventions, function(intervention) {
+        _.forEach($scope.selections.analysis.interventionInclusions, function(includedIntervention) {
+          if (includedIntervention.interventionId === intervention.id) {
+            $scope.selections.analysis.interventionsWithName.push(intervention);
+          }
+        });
+      });
+      $scope.selections.baselineTreatment = $scope.selections.analysis.interventionsWithName[0];
+
       selectedModelChanged();
     }
 
@@ -72,10 +84,10 @@ define(['lodash'], function(_) {
     function insertRelativeEffectsPlot() {
       if ($scope.selections.regressionLevel) {
         callback(ReportDirectiveService.getDirectiveBuilder('relative-effects-plot')($scope.selections.analysis.id,
-          $scope.selections.model.id, $scope.selections.baselineTreatment.interventionId, $scope.selections.regressionLevel));
+          $scope.selections.model.id, $scope.selections.baselineTreatment.id, $scope.selections.regressionLevel));
       } else {
         callback(ReportDirectiveService.getDirectiveBuilder('relative-effects-plot')($scope.selections.analysis.id,
-          $scope.selections.model.id, $scope.selections.baselineTreatment.interventionId));
+          $scope.selections.model.id, $scope.selections.baselineTreatment.id));
       }
       $modalInstance.close();
     }
