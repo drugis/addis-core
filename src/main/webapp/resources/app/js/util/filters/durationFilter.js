91c906f9df9f8045d0874de0baedf93f5a743c96
chinook25 <chinook25@gmail.com>
2017-04-25 14:02:32 +0200

    better duration filter

--- src/main/webapp/resources/app/js/util/filters/durationFilter.js
+++ src/main/webapp/resources/app/js/util/filters/durationFilter.js
@@ -12,9 +12,9 @@ define(['moment'], function(moment) {
       } else {
         var momentDuration = moment.duration(duration);
         if (momentDuration.asDays() >= 1) {
-          return momentDuration.asDays().toPrecision(3) + ' day(s)';
+          return parseFloat(momentDuration.asDays().toFixed(3)) + ' day(s)';
         } else {
-          return momentDuration.asHours().toPrecision(3) + ' hour(s)';
+          return parseFloat(momentDuration.asHours().toFixed(3)) + ' hour(s)';
         }
       }
     };
