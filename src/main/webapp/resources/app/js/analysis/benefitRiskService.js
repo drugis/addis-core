2c5597b1aaab337951e20cff55407c8de24d1cb6
Daan Reid <osmosisch@gmail.com>
2017-07-18 11:44:27 +0200

    fix BR input bugs

--- src/main/webapp/resources/app/js/analysis/benefitRiskService.js
+++ src/main/webapp/resources/app/js/analysis/benefitRiskService.js
@@ -3,6 +3,16 @@ define(['lodash'], function(_) {
   var dependencies = [];
   var BenefitRiskAnalysisService = function() {
 
+    function isMissingDataType(outcomesWithAnalyses) {
+      return _(outcomesWithAnalyses).filter('outcome.isIncluded').reject('dataType').value().length;
+    }
+
+    function isMissingAnalysis(outcomesWithAnalyses) {
+      return _.find(outcomesWithAnalyses, function(outcomeWithAnalyses) {
+        return outcomeWithAnalyses.dataType === 'network' && !outcomeWithAnalyses.selectedAnalysis;
+      });
+    }
+
     function addStudiesToOutcomes(outcomesWithAnalyses, studyInclusions, studies) {
       return _.map(outcomesWithAnalyses, function(outcomeWithAnalyses) {
         var outcomeWithAnalysesCopy = _.cloneDeep(outcomeWithAnalyses);
@@ -188,6 +198,8 @@ define(['lodash'], function(_) {
     }
 
     return {
+      isMissingDataType: isMissingDataType,
+      isMissingAnalysis: isMissingAnalysis,
       addModelsGroup: addModelsGroup,
       addStudiesToOutcomes: addStudiesToOutcomes,
       compareAnalysesByModels: compareAnalysesByModels,
