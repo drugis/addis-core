e083e074fe8bfbadf434a0bdf0675cf9bcb31142
chinook25 <chinook25@gmail.com>
2018-01-18 17:06:14 +0100

    excel export WIP building sheets

--- src/main/webapp/resources/app/js/excelExport/excelExportService.js
+++ src/main/webapp/resources/app/js/excelExport/excelExportService.js
@@ -7,7 +7,9 @@ define(['lodash', 'xlsx-shim', 'file-saver'], function(_, XLSX, saveAs) {
     'PopulationInformationService',
     'EpochService',
     'ActivityService',
-    'StudyDesignService'
+    'StudyDesignService',
+    'MeasurementMomentService',
+    'ConceptsService'
   ];
   var ExcelExportService = function($q, GROUP_ALLOCATION_OPTIONS, BLINDING_OPTIONS, STATUS_OPTIONS,
     StudyService,
@@ -16,7 +18,10 @@ define(['lodash', 'xlsx-shim', 'file-saver'], function(_, XLSX, saveAs) {
     PopulationInformationService,
     EpochService,
     ActivityService,
-    StudyDesignService) {
+    StudyDesignService,
+    MeasurementMomentService,
+    ConceptsService
+  ) {
 
     function exportStudy() {
       var promises = [StudyService.getJsonGraph()];
@@ -26,7 +31,9 @@ define(['lodash', 'xlsx-shim', 'file-saver'], function(_, XLSX, saveAs) {
         PopulationInformationService,
         EpochService,
         ActivityService,
-        StudyDesignService
+        StudyDesignService,
+        MeasurementMomentService,
+        ConceptsService
       ], function(service) {
         return service.queryItems();
       }));
@@ -40,27 +47,24 @@ define(['lodash', 'xlsx-shim', 'file-saver'], function(_, XLSX, saveAs) {
         var epochs = results[4];
         var activities = results[5];
         var studyDesign = results[6];
+        var measurementMoments = results[7];
+        var concepts = results[8];
 
         var workBook = XLSX.utils.book_new();
 
-        var studyDataSheet = buildStudyDataSheet();
-        var studyData = buildStudyInformation(study, studyInformation);
-
-        var armData = buildArmData(arms);
-        var armMerges = buildArmMerges(arms);
-
-        var treatmentLabels = buildTreatmentLabels(arms, epochs, activities, studyDesign);
-
-        var populationInformationData = buildPopulationInformationData(populationInformation);
-
-        _.merge(studyDataSheet, studyData, populationInformationData, armData, treatmentLabels);
-
-        studyDataSheet['!merges'] = studyDataSheet['!merges'].concat(armMerges);
-
+        var studyDataSheet = buildStudyDataSheet(study, studyInformation, arms, epochs, activities, studyDesign, populationInformation);
         var activitiesSheet = buildActivitiesSheet(activities);
+        var epochSheet = buildEpochSheet(epochs);
+        var studyDesignSheet = buildStudyDesignSheet(studyDesign, epochs, epochSheet, arms, studyDataSheet);
+        var measurementMomentSheet = buildMeasurementMomentSheet(measurementMoments, epochSheet);
+        var conceptSheet = buildConceptsSheet(concepts);
 
         XLSX.utils.book_append_sheet(workBook, studyDataSheet, 'Study data');
         XLSX.utils.book_append_sheet(workBook, activitiesSheet, 'Activities');
+        XLSX.utils.book_append_sheet(workBook, epochSheet, 'Epochs');
+        XLSX.utils.book_append_sheet(workBook, studyDesignSheet, 'Study design');
+        XLSX.utils.book_append_sheet(workBook, measurementMomentSheet, 'Measurement moments');
+        XLSX.utils.book_append_sheet(workBook, conceptSheet, 'Concepts');
         var workBookout = XLSX.write(workBook, {
           bookType: 'xlsx',
           type: 'array'
@@ -72,10 +76,164 @@ define(['lodash', 'xlsx-shim', 'file-saver'], function(_, XLSX, saveAs) {
     }
 
     //private
+    function buildConceptsSheet(concepts) {
+      var conceptsHeaders = {
+        A1: {
+          v: 'id'
+        },
+        B1: {
+          v: 'label'
+        },
+        C1: {
+          v: 'type'
+        },
+        D1: {
+          v: 'dataset concept uri'
+        },
+        E1: {
+          v: 'multiplier'
+        }
+      };
+      var conceptsData = _.reduce(concepts, function(accum, concept, index){
+        var row = index+2
+        accum['A'+row];
 
-    function buildStudyDataSheet() {
+        return accum;
+      },{});
+    }
+
+    function buildMeasurementMomentSheet(measurementMoments, epochSheet) {
+      var fromTypes = {
+        'ontology:anchorEpochStart': 'start',
+        'ontology:anchorEpochEnd': 'end'
+      };
+      var measurementMomentHeaders = {
+        A1: {
+          v: 'id'
+        },
+        B1: {
+          v: 'name'
+        },
+        C1: {
+          v: 'epoch'
+        },
+        D1: {
+          v: 'from'
+        },
+        E1: {
+          v: 'offset'
+        }
+      };
+      var measurementMomentData = _.reduce(measurementMoments, function(accum, measurementMoment, index) {
+        var row = index + 2;
+        accum['A' + row] = {
+          v: measurementMoment.uri
+        };
+        accum['B' + row] = {
+          v: measurementMoment.label
+        };
+        accum['C' + row] = {
+          f: '=Epochs!' + _.findKey(epochSheet, function(epochCell) {
+            return epochCell.v === measurementMoment.epochUri;
+          })
+        };
+        accum['D' + row] = {
+          v: fromTypes[measurementMoment.relativeToAnchor]
+        };
+        accum['E' + row] = {
+          v: measurementMoment.offset
+        };
+        return accum;
+      }, {});
+      return _.merge({
+        '!ref': 'A1:E' + (measurementMoments.length + 1)
+      }, measurementMomentHeaders, measurementMomentData);
+    }
+
+    function buildStudyDesignSheet(studyDesign, epochs, epochSheet, arms, studyDataSheet) {
+
+      var studyDesignSheet = _.merge({}, {
+        A1: {
+          v: 'arm'
+        }
+      }, _.reduce(epochs, function(accum, epoch, index) {
+        var epochReference = _.findKey(epochSheet, function(epochCell) {
+          return epochCell.v === epoch.label;
+        });
+        var column = XLSX.utils.encode_col(index + 1);
+        accum[column + 1] = {
+          f: '=Epochs!' + epochReference
+        };
+        return accum;
+      }, {}));
+      var lastColumn = XLSX.utils.encode_col(1 + epochs.length);
+      studyDesignSheet['!ref'] = 'A1:' + lastColumn + arms.length;
+      return studyDesignSheet;
+    }
+
+    function buildEpochSheet(epochs) {
+      var epochHeaders = {
+        A1: {
+          v: 'id'
+        },
+        B1: {
+          v: 'name'
+        },
+        C1: {
+          v: 'description'
+        },
+        D1: {
+          v: 'duration'
+        },
+        E1: {
+          v: 'Is primary?'
+        }
+      };
+      var epochData = _.reduce(epochs, function(accum, epoch, index) {
+        var row = index + 2;
+        accum['A' + row] = {
+          v: epoch.uri
+        };
+        accum['B' + row] = {
+          v: epoch.label
+        };
+        accum['C' + row] = {
+          v: epoch.comment
+        };
+        accum['D' + row] = {
+          v: epoch.duration
+        };
+        accum['E' + row] = {
+          v: epoch.isPrimary
+        };
+        return accum;
+      }, {});
+
+      return _.merge({
+        '!ref': 'A1:E' + (epochs.length + 1)
+      }, epochHeaders, epochData);
+    }
+
+    function buildStudyDataSheet(study, studyInformation, arms, epochs, activities, studyDesign, populationInformation) {
+      var studyDataSheet = initStudyDataSheet();
+      var studyData = buildStudyInformation(study, studyInformation);
+      var armData = buildArmData(arms);
+      var treatmentLabels = buildTreatmentLabels(arms, epochs, activities, studyDesign);
+      var populationInformationData = buildPopulationInformationData(populationInformation);
+
+      var armMerges = buildArmMerges(arms);
+
+      _.merge(studyDataSheet, studyData, populationInformationData, armData, treatmentLabels);
+      studyDataSheet['!merges'] = studyDataSheet['!merges'].concat(armMerges);
+      var measurementCounter = 30; // placeholder FIXME once measurements are implemented
+      studyDataSheet['!ref'] = 'A1:' +
+        XLSX.utils.encode_col(11 + measurementCounter) +
+        (3 + arms.length);
+      return studyDataSheet;
+    }
+
+    function initStudyDataSheet() {
       var studyDataSheet = {
-        '!ref': 'A1:N10', //fixme
         A1: { //row 1
           v: 'Study Information'
         },
