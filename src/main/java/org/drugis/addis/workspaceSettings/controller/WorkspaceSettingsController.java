1d1d68f89f28c33cff2d1705bc2998704bf3873c
chinook25 <chinook25@gmail.com>
2018-08-23 10:41:36 +0200

    MCDA settings

--- src/main/java/org/drugis/addis/workspaceSettings/controller/WorkspaceSettingsController.java
+++ src/main/java/org/drugis/addis/workspaceSettings/controller/WorkspaceSettingsController.java
@@ -4,7 +4,6 @@ import org.drugis.addis.base.AbstractAddisCoreController;
 import org.drugis.addis.exception.MethodNotAllowedException;
 import org.drugis.addis.exception.ResourceDoesNotExistException;
 import org.drugis.addis.projects.service.ProjectService;
-import org.drugis.addis.workspaceSettings.WorkspaceSettings;
 import org.drugis.addis.workspaceSettings.repository.WorkspaceSettingsRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -23,15 +22,15 @@ public class WorkspaceSettingsController extends AbstractAddisCoreController {
 
   @RequestMapping(value = "/projects/{projectId}/analyses/{analysisId}/workspaceSettings", method = RequestMethod.GET)
   @ResponseBody
-  public WorkspaceSettings get(@PathVariable Integer analysisId) {
-    return workspaceSettingsRepository.get(analysisId);
+  public String get(@PathVariable Integer analysisId) {
+    return workspaceSettingsRepository.get(analysisId).getSettings();
   }
 
   @RequestMapping(value = "/projects/{projectId}/analyses/{analysisId}/workspaceSettings", method = RequestMethod.PUT)
   @ResponseBody
   public void put(Principal principal, @PathVariable Integer projectId, @PathVariable Integer analysisId,
-                  @RequestBody String workspaceSettings) throws ResourceDoesNotExistException, MethodNotAllowedException {
+                  @RequestBody String settings) throws ResourceDoesNotExistException, MethodNotAllowedException {
     projectService.checkOwnership(projectId, principal);
-    workspaceSettingsRepository.put(analysisId, workspaceSettings);
+    workspaceSettingsRepository.put(analysisId, settings);
   }
 }
