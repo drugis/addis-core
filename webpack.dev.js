e1bc3f7686aad255472838ef1ef9ca449cb09af7
Daan Reid <osmosisch@gmail.com>
2020-09-16 11:48:17 +0200

    More upgrades

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,24 +1,24 @@
-'use strict';
+"use strict";
 
-const {merge} = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+//   .BundleAnalyzerPlugin;
 
 let config = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader']
-      }
-    ]
+        loaders: ["style-loader", "css-loader"],
+      },
+    ],
   },
   plugins: [
     //    new BundleAnalyzerPlugin()
-  ]
+  ],
 });
 module.exports = config;
